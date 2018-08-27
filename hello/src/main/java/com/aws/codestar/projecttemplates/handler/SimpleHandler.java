b2473318a24ab526956f9334c4393e20aebda6e3
Orestis Gkorgkas <og@bibsys.no>
2018-08-27 16:05:22 +0200

    wip: simple handler

--- hello/src/main/java/com/aws/codestar/projecttemplates/handler/SimpleHandler.java
+++ hello/src/main/java/com/aws/codestar/projecttemplates/handler/SimpleHandler.java
@@ -3,13 +3,18 @@ package com.aws.codestar.projecttemplates.handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.aws.codestar.projecttemplates.GatewayResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.rmi.runtime.Log;
 
 
 public class SimpleHandler implements RequestHandler<SimpleRequest, SimpleResponse> {
 
+  public static final Logger logger=LoggerFactory.getLogger(SimpleHandler.class);
+
   @Override
   public SimpleResponse handleRequest(SimpleRequest input, Context context) {
-
+    logger.info(input.getClass().getName());
     String name=input.getName();
     Integer age=input.getAge();
     String message=String.format("Hello %s!, Are you %d years old?",name,age);
