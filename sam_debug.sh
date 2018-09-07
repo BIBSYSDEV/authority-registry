43288b8e6bbf2552c3f87a1385a6845fc066f541
Orestis Gkorgkas <og@bibsys.no>
2018-09-07 12:13:01 +0200

    feat: scripts for building and debugging

--- sam_debug.sh
+++ sam_debug.sh
@@ -3,11 +3,18 @@
 method=POST
 echo $#
 if [ $# -eq 0  ]; then 
- printf "%s\n%s\n%s\n" "Argument 1: file with the request body " "Argument 2: Request method:(GET POST etc)" "Argument 3: Resource name from template.yaml of the API lambda function to be invoked" 
+ printf "%s\n%s\n%s\n" "Argument 1: file with the request body " "Argument 2: Request method:(GET POST etc)" \
+ "Argument 3: Resource name from template.yaml of the API lambda function to be invoked" \
+ "Argument 4 (Optional):Debug port"  
 else 
   if [ "$#" -eq 2 ]; then
     method=$2
   fi
+    cat  $1 |  sed -e "s/^/'/g" | sed -e "s/$/'/g" | xargs sam local generate-event api -m $method -b
 
-  cat  $1 |  sed -e "s/^/'/g" | sed -e "s/$/'/g" |sam local generate-event api -m $method | sam  local invoke $3
+  if [ $# -eq 4 ]; then
+    cat  $1 |  sed -e "s/^/'/g" | sed -e "s/$/'/g" |xargs  sam local generate-event api -m $method -b| sam  local invoke --debug-port $4 $3
+  else
+    cat  $1 |  sed -e "s/^/'/g" | sed -e "s/$/'/g" |xargs sam local generate-event api -m $method -b | sam  local invoke  $3
+  fi
 fi
