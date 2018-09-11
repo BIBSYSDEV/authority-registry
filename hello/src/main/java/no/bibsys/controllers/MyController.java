4050d673dd5a25f352860ba4c21a27c2c1e5704b
Orestis Gkorgkas <og@bibsys.no>
2018-09-11 15:14:52 +0200

    wip: copy Controller

--- hello/src/main/java/no/bibsys/controllers/MyController.java
+++ hello/src/main/java/no/bibsys/controllers/MyController.java
@@ -1,6 +1,5 @@
 package no.bibsys.controllers;
 
-
 import no.bibsys.responses.SimpleResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
