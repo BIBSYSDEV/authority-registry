1c4c762e74e144e8a885a86cfa3231416194309c
Orestis Gkorgkas <og@bibsys.no>
2018-09-11 14:45:50 +0200

    wip: should work but does not

--- hello/src/main/java/no/bibsys/controllers/MyController.java
+++ hello/src/main/java/no/bibsys/controllers/MyController.java
@@ -1,11 +1,9 @@
 package no.bibsys.controllers;
 
-import no.bibsys.handlers.requests.SimpleRequest;
 import no.bibsys.handlers.responses.SimpleResponse;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
