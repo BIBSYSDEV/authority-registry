3e26584ee3cefe2e6f9a9b7d705afc980951db18
Orestis Gkorgkas <og@bibsys.no>
2018-09-13 17:10:38 +0200

    ref: minor refactoring

--- hello/src/test/java/no/bibsys/LocalDynamoConfiguration.java
+++ hello/src/test/java/no/bibsys/LocalDynamoConfiguration.java
@@ -3,20 +3,23 @@ package no.bibsys;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded;
-import no.bibsys.controllers.MyController;
+import no.bibsys.controllers.DatabaseController;
+import no.bibsys.controllers.DatabaseControllerExcepctionHandler;
 import no.bibsys.db.DatabaseManager;
 import no.bibsys.db.TableCreator;
 import no.bibsys.db.TableDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(DatabaseControllerExcepctionHandler.class)
 public class LocalDynamoConfiguration {
 
 
   @Bean
-  public MyController getController(DatabaseManager databaseManager) {
-    return new MyController(databaseManager);
+  public DatabaseController getController(DatabaseManager databaseManager) {
+    return new DatabaseController(databaseManager);
   }
 
   @Bean
