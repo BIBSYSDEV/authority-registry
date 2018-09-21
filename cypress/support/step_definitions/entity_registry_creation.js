b4c5572dc8cb265d0daedb876ef13bdffa54ec0e
Eirik Nilsen <eirik.nilsen@deneb.no>
2018-09-21 09:05:34 +0200

    Simplification of entity creation test

--- cypress/support/step_definitions/entity_registry_creation.js
+++ cypress/support/step_definitions/entity_registry_creation.js
@@ -7,41 +7,34 @@
 //    Then an entity registry that accepts only valid data is created
 
 let createEntityRegistryRequest ={
-		name: "registry name",
-		adminUsers: ["user1", "user2"],
-		validationSchema: "schema"
+		'Registry name': 'test',
+		'Registry admin users': ['user1', 'user2'],
+		'Registry validation schema': 'schema'
 }
 
-function validateSchema(schema){
-	// schema validation here
-	if(schema){
-		return true
-	}
-	
-	return false
-}
+let createRegistryEndpoint = 'http://ada.bibsys.no/admin/ping' // use create registry lambda when ready
 
-let createRegistryEndpoint = "http://ada.bibsys.no/admin/ping" // use create registry lambda when ready
-
-when('the {userType} user provides a properly formatted create-entity-registry-request providing information about:', (user, dataTable) =>{
+when('the API admin user provides a properly formatted create-entity-registry-request providing information about:', (dataTable) =>{
 	
-//	expect(dataTable.rawTable[0]).to.equal("Registry name")
-	expect(user).to.equal("API admin")
+	let attributeArray = dataTable.rawTable;
 	
-	expect(createEntityRegistryRequest.name).to.not.be.undefined;
-	expect(createEntityRegistryRequest.name).to.be.a('string');
-	expect(createEntityRegistryRequest.name).to.have.length.above(0);
+	expect(createEntityRegistryRequest[attributeArray[0]]).to.be.a('string');
+	expect(createEntityRegistryRequest[attributeArray[0]]).to.have.length.above(0);
 
-	expect(createEntityRegistryRequest.adminUsers).to.not.be.undefined;
-	expect(createEntityRegistryRequest.adminUsers).to.be.a('array');
-	expect(createEntityRegistryRequest.adminUsers).to.have.length.above(0);
-	expect(createEntityRegistryRequest.adminUsers[0]).to.have.length.above(0);
+	expect(createEntityRegistryRequest[attributeArray[1]]).to.be.a('array');
+	expect(createEntityRegistryRequest[attributeArray[1]]).to.have.length.above(0);
+	expect(createEntityRegistryRequest[attributeArray[1]][0]).to.be.a('string');
+	expect(createEntityRegistryRequest[attributeArray[1]][0]).to.have.length.above(0);
 
-	expect(createEntityRegistryRequest.validationSchema).to.not.be.undefined;
-	expect(createEntityRegistryRequest.validationSchema).to.be.a('string');
-	expect(createEntityRegistryRequest.validationSchema).to.have.length.above(0);
-	
-	expect(validateSchema(createEntityRegistryRequest.validationSchema)).to.be.true;
+	expect(createEntityRegistryRequest[attributeArray[2]]).to.be.a('string');
+	expect(createEntityRegistryRequest[attributeArray[2]]).to.have.length.above(0);
+
+	let schemaValidationUrl = 'http://ada.bibsys.no/admin/ping';
+	cy.request(schemaValidationUrl, createEntityRegistryRequest[attributeArray[2]])
+	.then((response) => {
+		expect(true).to.be.true;
+	})
+
 })
 
 then('an entity registry that accepts only valid data is created', () =>{
