c3307b2cdac08437abc2f525d782a59dad8afd98
Orestis Gkorgkas <og@bibsys.no>
2018-09-21 12:58:43 +0200

    feat: PathResponse for sending REST pats in responses

---
+++ api/src/main/java/no/bibsys/responses/PathResponse.java
@@ -0,0 +1,41 @@
+package no.bibsys.responses;
+
+public class PathResponse {
+
+    private String path;
+
+
+    public PathResponse() {
+    }
+
+    public PathResponse(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PathResponse)) {
+            return false;
+        }
+
+        PathResponse that = (PathResponse) o;
+
+        return path != null ? path.equals(that.path) : that.path == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return path != null ? path.hashCode() : 0;
+    }
+}
