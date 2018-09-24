8fd50acf802af74257344eec124ac19ffe5212e4
Orestis Gkorgkas <og@bibsys.no>
2018-09-24 12:52:13 +0200

    fix: make constant final

--- dynamo/src/main/java/no/bibsys/db/DynamoConstants.java
+++ dynamo/src/main/java/no/bibsys/db/DynamoConstants.java
@@ -2,7 +2,7 @@ package no.bibsys.db;
 
 public class DynamoConstants {
 
-    public static String KEY_NOT_EXISTS = "attribute_not_exists(id)";
+    public static final String KEY_NOT_EXISTS = "attribute_not_exists(id)";
 
 
 }
