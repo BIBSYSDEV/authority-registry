312448be3bcb3da3e129ae78de9d7bbead6dea7e
Eirik Nilsen <eirik.nilsen@deneb.no>
2018-10-16 11:38:26 +0200

    Add registry information to schema table

--- dynamo/src/main/java/no/bibsys/db/TableManager.java
+++ dynamo/src/main/java/no/bibsys/db/TableManager.java
@@ -11,9 +11,6 @@ import no.bibsys.db.structures.ValidationSchemaEntry;
 
 public class TableManager {
 
-    public static final String VALIDATION_SCHEMA_TABLE = "VALIDATION_SCHEMAS";
-
-
     private final transient TableDriver tableDriver;
 
 
@@ -24,8 +21,6 @@ public class TableManager {
     public void deleteTable(final String tableName) throws InterruptedException {
         try {
             tableDriver.deleteTable(tableName);
-            TableWriter writer = new TableWriter(tableDriver, VALIDATION_SCHEMA_TABLE);
-            writer.deleteEntry(tableName);
         } catch (ResourceNotFoundException e) {
             throw new TableNotFoundException(e.getMessage());
         }
@@ -51,41 +46,18 @@ public class TableManager {
      * This method does not delete the validation schema of the table
      */
     public void emptyTable(final String tableName) throws InterruptedException {
-        if (!tableName.equals(VALIDATION_SCHEMA_TABLE) && tableDriver.tableExists(tableName)) {
+        if (tableDriver.tableExists(tableName)) {
             tableDriver.deleteNoCheckTable(tableName);
         } else {
             throw new TableNotFoundException(tableName);
         }
     }
 
-
-    private void createValidationSchemaTable() throws InterruptedException {
-        tableDriver.createTable(VALIDATION_SCHEMA_TABLE, new ValidationSchemaEntry());
-    }
-
-
     public void createRegistry(String tableName, String validationSchema)
         throws InterruptedException, JsonProcessingException {
-        if (!tableDriver.tableExists(VALIDATION_SCHEMA_TABLE)) {
-            createValidationSchemaTable();
-        }
-        insertValidationSchema(tableName, validationSchema);
-
         tableDriver.createTable(tableName);
 
     }
-
-
-    private void insertValidationSchema(String tableName, String validationSchema)
-        throws JsonProcessingException {
-        TableWriter tableWriter = new TableWriter(tableDriver, VALIDATION_SCHEMA_TABLE);
-        try {
-            tableWriter.insertEntry(new ValidationSchemaEntry(tableName, validationSchema));
-        } catch (ItemExistsException e) {
-            throw new TableAlreadyExistsException(e.getMessage());
-        }
-    }
-
 
     public boolean tableExists(String tableName){
         return this.tableDriver.tableExists(tableName);
