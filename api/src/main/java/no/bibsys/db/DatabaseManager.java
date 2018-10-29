730595df15b634f8050954f3252cf6a00ffc7677
Eirik Nilsen <eirik.nilsen@unit.no>
2018-10-29 07:49:35 +0100

    More logging

--- api/src/main/java/no/bibsys/db/DatabaseManager.java
+++ api/src/main/java/no/bibsys/db/DatabaseManager.java
@@ -1,15 +1,21 @@
 package no.bibsys.db;
 
 import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.amazonaws.services.dynamodbv2.model.TableAlreadyExistsException;
 import com.amazonaws.services.dynamodbv2.model.TableNotFoundException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+
 import no.bibsys.db.structures.EntityRegistryTemplate;
 import no.bibsys.web.model.EditRegistryRequest;
 
 public class DatabaseManager {
 
     private final transient TableDriver tableDriver;
+    private static final transient Logger logger = LoggerFactory.getLogger(DatabaseManager.class);
 
     public DatabaseManager(TableDriver tableDriver) {
         this.tableDriver = tableDriver;
@@ -70,12 +76,15 @@ public class DatabaseManager {
 
     public void deleteRegistry(String tableName) {
         TableManager tableManager = new TableManager(tableDriver);
+        logger.debug(String.format("Deleting table %s", tableName));
         if (registryExists(tableName)) {
-            
+            logger.debug(String.format("Deleting table %s from schema table", tableName));
             TableWriter schemaTableWriter = new TableWriter(tableDriver, TableManager.VALIDATION_SCHEMA_TABLE);
             schemaTableWriter.deleteEntry(tableName);
+            logger.debug(String.format("Schema table cleared"));
             
             tableManager.deleteTable(tableName);
+            logger.debug(String.format("Table %s deleted", tableName));
         } else {
             throw new TableNotFoundException(String.format("Registry %s does not exist", tableName));
         }
