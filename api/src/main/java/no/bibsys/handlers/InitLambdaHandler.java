cfc920d3c34469fbf1c24ccc2c6fa5adcb842b9d
Jan Bjørndalen <jan.bjorndalen@gmail.com>
2018-11-05 14:32:19 +0100

    AUTREG-66 Using DynamoDBMapper

--- api/src/main/java/no/bibsys/handlers/InitLambdaHandler.java
+++ api/src/main/java/no/bibsys/handlers/InitLambdaHandler.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig.TableNameOverride;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
@@ -16,14 +19,16 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import no.bibsys.EnvironmentReader;
 import no.bibsys.web.security.ApiKeyConstants;
+import no.bibsys.web.security.Roles;
 
 public class InitLambdaHandler implements RequestHandler<String, String> {
 
     private final transient String apiKeyTableName;
+    private final transient AmazonDynamoDB client;
     private final transient DynamoDB dynamoDB;
     
     public InitLambdaHandler() {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        client = AmazonDynamoDBClientBuilder.standard().build();
         dynamoDB = new DynamoDB(client);
         apiKeyTableName = new EnvironmentReader().getEnvForName(ApiKeyConstants.API_KEY_TABLE_NAME).orElse("entity-registry-api-keys");
     }
@@ -34,9 +39,6 @@ public class InitLambdaHandler implements RequestHandler<String, String> {
     public String handleRequest(String input, Context context) {
         List<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
         attributeDefinitions.add(new AttributeDefinition().withAttributeName("Key").withAttributeType("S"));
-        attributeDefinitions.add(new AttributeDefinition().withAttributeName("Roles").withAttributeType("SS"));
-        attributeDefinitions.add(new AttributeDefinition().withAttributeName("Active").withAttributeType("BOOL"));
-        attributeDefinitions.add(new AttributeDefinition().withAttributeName("Modified").withAttributeType("S"));
         
         List<KeySchemaElement> keySchema = new ArrayList<KeySchemaElement>();
         keySchema.add(new KeySchemaElement().withAttributeName("Key").withKeyType(KeyType.HASH));
@@ -55,6 +57,11 @@ public class InitLambdaHandler implements RequestHandler<String, String> {
         } catch (InterruptedException e) {
 
         }
+        
+        DynamoDBMapper mapper = new DynamoDBMapper(client);
+        DynamoDBMapperConfig config = DynamoDBMapperConfig.builder().withTableNameOverride(TableNameOverride.withTableNameReplacement(apiKeyTableName)).build();
+        ApiKey apiKey = new ApiKey(Roles.REGISTRY_ADMIN);
+        mapper.save(apiKey, config);
         
         return table.getTableName();
 
