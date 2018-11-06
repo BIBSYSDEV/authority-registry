d6a7edd97265e82f496573d19362e0c12698985a
Eirik Nilsen <eirik.nilsen@unit.no>
2018-11-06 12:21:34 +0100

    fix: constructors

--- api/src/main/java/no/bibsys/web/model/GatewayResponse.java
+++ api/src/main/java/no/bibsys/web/model/GatewayResponse.java
@@ -22,51 +22,50 @@ public class GatewayResponse {
     public GatewayResponse(final String body, final Map<String, String> headers, final int statusCode) {
         this.statusCode = statusCode;
         this.body = body;
-   public GatewayResponse(final String body, final int statusCode){
-         this(body, defaultHeaders(),statusCode)
-   }
-            this.headers = Collections.unmodifiableMap(defaultHeaders());
-        }else {
-            this.headers = Collections.unmodifiableMap(new HashMap<>(headers));
-        }
+
+        this.headers = Collections.unmodifiableMap(defaultHeaders());
+    }
+    
+    public GatewayResponse(final String body, final int statusCode){
+        this(body, defaultHeaders(),statusCode);
     }
 
-    public GatewayResponse(final String body) {
-        this.body = body;
-        Map<String, String> map = defaultHeaders();
-        this.headers = Collections.unmodifiableMap(map);
-        this.statusCode = 200;
-    }
+public GatewayResponse(final String body) {
+    this.body = body;
+    Map<String, String> map = defaultHeaders();
+    this.headers = Collections.unmodifiableMap(map);
+    this.statusCode = 200;
+}
 
 
-    public static Map<String, String> defaultHeaders() {
-        Map<String, String> map = new ConcurrentHashMap<>();
-        map.put("Content-Type", "application/json");
-        return map;
-    }
+public static Map<String, String> defaultHeaders() {
+    Map<String, String> map = new ConcurrentHashMap<>();
+    map.put("Content-Type", "application/json");
+    return map;
+}
 
 
-    public String getBody() {
-        return body;
-    }
+public String getBody() {
+    return body;
+}
 
-    public void setBody(String body) {
-        this.body = body;
-    }
+public void setBody(String body) {
+    this.body = body;
+}
 
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
+public Map<String, String> getHeaders() {
+    return headers;
+}
 
-    public void setHeaders(Map<String, String> headers) {
-        this.headers = headers;
-    }
+public void setHeaders(Map<String, String> headers) {
+    this.headers = headers;
+}
 
-    public int getStatusCode() {
-        return statusCode;
-    }
+public int getStatusCode() {
+    return statusCode;
+}
 
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
+public void setStatusCode(int statusCode) {
+    this.statusCode = statusCode;
+}
 }
