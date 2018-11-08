ece89fb65501a9e91709bd29b25906d2ce2f38ea
Orestis Gkorgkas <og@bibsys.no>
2018-11-08 15:25:25 +0100

    fix: update init lambda handler to create new key tables

--- api/src/main/java/no/bibsys/handlers/InitHandler.java
+++ api/src/main/java/no/bibsys/handlers/InitHandler.java
@@ -1,20 +1,38 @@
 package no.bibsys.handlers;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import no.bibsys.EnvironmentReader;
 import no.bibsys.amazon.handlers.events.CodePipelineEvent;
 import no.bibsys.amazon.handlers.responses.SimpleResponse;
 import no.bibsys.amazon.handlers.templates.CodePipelineFunctionHandlerTemplate;
+import no.bibsys.service.AuthenticationService;
 
 public class InitHandler extends CodePipelineFunctionHandlerTemplate<SimpleResponse> {
+
+
+    private final transient AuthenticationService authenticationService;
+
+    public InitHandler() {
+        final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        authenticationService = new AuthenticationService(client, new EnvironmentReader());
+    }
+
+
 
     @Override
     public SimpleResponse processInput(CodePipelineEvent input, Context context)
         throws IOException, URISyntaxException {
 
         String pipelineId = input.getId();
-        return new SimpleResponse(pipelineId);
+
+        String tableName = authenticationService.createApiKeyTable();
+        authenticationService.setUpInitialApiKeys();
+
+        return new SimpleResponse(tableName);
 
 
     }
