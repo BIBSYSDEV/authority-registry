6b04543c459e2e588d4943d2c38299297009b417
Eirik Nilsen <eirik.nilsen@unit.no>
2018-11-19 13:18:20 +0100

    Removed unused imports

--- api/src/main/java/no/bibsys/web/DatabaseResource.java
+++ api/src/main/java/no/bibsys/web/DatabaseResource.java
@@ -18,7 +18,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response.Status;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
