dd92e023d8b0d62df70f774fb2109b2e9d15d8be
Eirik Nilsen <eirik.nilsen@unit.no>
2018-11-21 08:13:47 +0100

    fix: import

--- api/src/main/java/no/bibsys/web/DatabaseResource.java
+++ api/src/main/java/no/bibsys/web/DatabaseResource.java
@@ -16,6 +16,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
