ff59d057ab8bb12abbbd6f13b3a7dcbefb54fbf7
Orestis Gkorgkas <og@bibsys.no>
2018-11-28 11:16:57 +0100

    wip: UrlUpdater

--- api/src/main/java/no/bibsys/staticurl/UrlUpdater.java
+++ api/src/main/java/no/bibsys/staticurl/UrlUpdater.java
@@ -1,11 +1,57 @@
 package no.bibsys.staticurl;
 
+import com.amazonaws.services.apigateway.AmazonApiGateway;
+import com.amazonaws.services.route53.model.ChangeResourceRecordSetsRequest;
+import java.util.Optional;
+import no.bibsys.aws.cloudformation.Stage;
+import no.bibsys.aws.git.github.GitInfo;
+import no.bibsys.aws.route53.Route53Updater;
+import no.bibsys.aws.route53.StaticUrlInfo;
+
 public class UrlUpdater {
 
 
-    public void foo() {
+    public final static String DEFAULT_ZONE_NAME = "aws.unit.no";
+    public final static String DEFAULT_RECORD_SET_NAME = "api.entitydata.aws.unit.no.";
+
+    private final transient String zoneName;
+    private final transient String recordSetName;
+    private final transient GitInfo gitInfo;
+    private final transient AmazonApiGateway apiGatewayClient;
+    private final transient String certificateArn;
+
+    public UrlUpdater(String zoneName,
+        String recordSetName,
+        GitInfo gitInfo,
+        AmazonApiGateway apiGatewayClient,
+        String certificateArn) {
+        this.zoneName = zoneName;
+        this.recordSetName = recordSetName;
+        this.gitInfo = gitInfo;
+        this.apiGatewayClient = apiGatewayClient;
+        this.certificateArn = certificateArn;
+    }
+
+    private StaticUrlInfo staticUrlInfo(Stage stage) {
+        return StaticUrlInfo.create(stage, zoneName, recordSetName);
+    }
+
+    public Optional<ChangeResourceRecordSetsRequest> updateUrlMapping(Stage stage,
+        String restApiPhysicalId) {
+
+        Route53Updater route53Updater = new Route53Updater(
+            staticUrlInfo(stage),
+            gitInfo,
+            stage,
+            restApiPhysicalId,
+            apiGatewayClient
+        );
+
+        return route53Updater.createUpdateRequest(certificateArn);
+
 
     }
+
 
 
 }
--- api/src/test/java/no/bibsys/staticurl/UrlUpdaterTest.java
+++ api/src/test/java/no/bibsys/staticurl/UrlUpdaterTest.java
@@ -1,12 +1,51 @@
 package no.bibsys.staticurl;
 
+import static org.junit.Assert.assertTrue;
+
+import com.amazonaws.services.apigateway.AmazonApiGateway;
+import com.amazonaws.services.route53.model.ChangeResourceRecordSetsRequest;
+import java.util.Optional;
+import no.bibsys.aws.cloudformation.Stage;
+import no.bibsys.aws.git.github.GitInfoImpl;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class UrlUpdaterTest {
 
+    private final transient String branchName = "TheBranch";
+    private final transient String ownerNamer = "TheOwner";
+    private final transient String repository = "TheRepo";
+    private final transient String certificateArn = "TheCerificate";
+
+    private transient UrlUpdater urlUpdater;
+
+
+    public UrlUpdaterTest() {
+        GitInfoImpl gitInfo = new GitInfoImpl(ownerNamer, repository, branchName);
+        AmazonApiGateway client = mockClient();
+        urlUpdater = new UrlUpdater(
+            UrlUpdater.DEFAULT_ZONE_NAME,
+            UrlUpdater.DEFAULT_RECORD_SET_NAME,
+            gitInfo,
+            client,
+            certificateArn
+        );
+
+
+    }
+
+
+    private AmazonApiGateway mockClient() {
+        return Mockito.mock(AmazonApiGateway.class);
+    }
 
     @Test
-    public void UrlUpdaterShouldCreate() {
+    public void urlUpdater_stage_restApiId() {
+
+        Optional<ChangeResourceRecordSetsRequest> result = urlUpdater
+            .updateUrlMapping(Stage.TEST, "SomeAPi");
+
+        assertTrue(result.isPresent());
 
     }
 
