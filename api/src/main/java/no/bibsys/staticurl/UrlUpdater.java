4b70eeef856ecb36f158a1666b32134c0c7ba47d
Orestis Gkorgkas <og@bibsys.no>
2018-11-29 10:17:39 +0100

    ref: split creation and execution of update request

--- api/src/main/java/no/bibsys/staticurl/UrlUpdater.java
+++ api/src/main/java/no/bibsys/staticurl/UrlUpdater.java
@@ -1,12 +1,9 @@
 package no.bibsys.staticurl;
 
-import com.amazonaws.services.apigateway.AmazonApiGateway;
 import com.amazonaws.services.route53.model.ChangeResourceRecordSetsRequest;
+import com.amazonaws.services.route53.model.ChangeResourceRecordSetsResult;
 import java.util.Optional;
-import no.bibsys.aws.cloudformation.Stage;
-import no.bibsys.aws.git.github.GitInfo;
 import no.bibsys.aws.route53.Route53Updater;
-import no.bibsys.aws.route53.StaticUrlInfo;
 
 public class UrlUpdater {
 
@@ -14,42 +11,25 @@ public class UrlUpdater {
     public final static String DEFAULT_ZONE_NAME = "aws.unit.no";
     public final static String DEFAULT_RECORD_SET_NAME = "api.entitydata.aws.unit.no.";
 
-    private final transient String zoneName;
-    private final transient String recordSetName;
-    private final transient GitInfo gitInfo;
-    private final transient AmazonApiGateway apiGatewayClient;
+
+    private final transient Route53Updater route53Updater;
     private final transient String certificateArn;
 
-    public UrlUpdater(String zoneName,
-        String recordSetName,
-        GitInfo gitInfo,
-        AmazonApiGateway apiGatewayClient,
+    public UrlUpdater(
+        Route53Updater route53Updater,
         String certificateArn) {
-        this.zoneName = zoneName;
-        this.recordSetName = recordSetName;
-        this.gitInfo = gitInfo;
-        this.apiGatewayClient = apiGatewayClient;
+
+        this.route53Updater = route53Updater;
         this.certificateArn = certificateArn;
     }
 
-    private StaticUrlInfo staticUrlInfo(Stage stage) {
-        return StaticUrlInfo.create(stage, zoneName, recordSetName);
-    }
-
-    public Optional<ChangeResourceRecordSetsRequest> updateUrlMapping(Stage stage,
-        String restApiPhysicalId) {
-
-        Route53Updater route53Updater = new Route53Updater(
-            staticUrlInfo(stage),
-            gitInfo,
-            stage,
-            restApiPhysicalId,
-            apiGatewayClient
-        );
-
+    public Optional<ChangeResourceRecordSetsRequest> createUpdateRequest() {
         return route53Updater.createUpdateRequest(certificateArn);
 
+    }
 
+    public ChangeResourceRecordSetsResult executeUpdate(ChangeResourceRecordSetsRequest request) {
+        return route53Updater.executeUpdateRequest(request);
     }
 
 
