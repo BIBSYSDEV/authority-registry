026adac411fc009cfc8270d49090af8e7ff13c7e
Orestis Gkorgkas <og@bibsys.no>
2018-11-29 10:17:12 +0100

    feat: UrlUpdater tests

--- api/src/test/java/no/bibsys/staticurl/UrlUpdaterTest.java
+++ api/src/test/java/no/bibsys/staticurl/UrlUpdaterTest.java
@@ -1,52 +1,122 @@
 package no.bibsys.staticurl;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 import com.amazonaws.services.apigateway.AmazonApiGateway;
+import com.amazonaws.services.apigateway.model.BasePathMapping;
+import com.amazonaws.services.apigateway.model.GetBasePathMappingsResult;
+import com.amazonaws.services.apigateway.model.GetDomainNameResult;
+import com.amazonaws.services.certificatemanager.model.RecordType;
+import com.amazonaws.services.route53.AmazonRoute53;
 import com.amazonaws.services.route53.model.ChangeResourceRecordSetsRequest;
+import com.amazonaws.services.route53.model.HostedZone;
+import com.amazonaws.services.route53.model.ListHostedZonesResult;
+import com.amazonaws.services.route53.model.ResourceRecordSet;
 import java.util.Optional;
 import no.bibsys.aws.cloudformation.Stage;
-import no.bibsys.aws.git.github.GitInfoImpl;
+import no.bibsys.aws.route53.Route53Updater;
+import no.bibsys.aws.route53.StaticUrlInfo;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class UrlUpdaterTest {
-
-    private final transient String branchName = "TheBranch";
-    private final transient String ownerNamer = "TheOwner";
-    private final transient String repository = "TheRepo";
     private final transient String certificateArn = "TheCerificate";
+    private final transient String hostedZoneId = "HOSTEDZONEID";
+    private final transient String domainName = "DomainName";
 
+    private final transient StaticUrlInfo staticUrlInfo;
     private transient UrlUpdater urlUpdater;
 
+    private transient ResourceRecordSet recordSet;
 
     public UrlUpdaterTest() {
-        GitInfoImpl gitInfo = new GitInfoImpl(ownerNamer, repository, branchName);
-        AmazonApiGateway client = mockClient();
-        urlUpdater = new UrlUpdater(
+        staticUrlInfo = new StaticUrlInfo(
             UrlUpdater.DEFAULT_ZONE_NAME,
             UrlUpdater.DEFAULT_RECORD_SET_NAME,
-            gitInfo,
-            client,
-            certificateArn
-        );
+            Stage.TEST);
+        AmazonApiGateway client = mockApiGatewayClient();
+
+        Route53Updater route53Updater = new Route53Updater(staticUrlInfo,
+            "apiID",
+            client);
+
+        route53Updater.setRoute53Client(mockRoute53Client());
+        urlUpdater = new UrlUpdater(route53Updater, certificateArn);
+
+
+    }
+
+    @Before
+    public void generateRecordSet() {
+        Optional<ChangeResourceRecordSetsRequest> requestOpt = urlUpdater.createUpdateRequest();
+
+        assertTrue(requestOpt.isPresent());
+        ChangeResourceRecordSetsRequest request = requestOpt.get();
+        assertThat(request.getHostedZoneId(), is(equalTo(hostedZoneId)));
+        assertThat(request.getChangeBatch().getChanges().size(), is(equalTo(1)));
+
+        this.recordSet = request.getChangeBatch()
+            .getChanges()
+            .get(0).getResourceRecordSet();
 
 
     }
 
 
-    private AmazonApiGateway mockClient() {
-        return Mockito.mock(AmazonApiGateway.class);
+    private AmazonRoute53 mockRoute53Client() {
+        AmazonRoute53 client = Mockito.mock(AmazonRoute53.class);
+        ListHostedZonesResult mockResult = new ListHostedZonesResult()
+            .withHostedZones(
+                new HostedZone().withId(hostedZoneId).withName(UrlUpdater.DEFAULT_ZONE_NAME));
+        when(client.listHostedZones()).thenReturn(mockResult);
+        return client;
+    }
+
+
+    private AmazonApiGateway mockApiGatewayClient() {
+        AmazonApiGateway client = Mockito.mock(AmazonApiGateway.class);
+        when(client.getDomainName(any()))
+            .thenReturn(new GetDomainNameResult().withRegionalDomainName(domainName));
+
+        // necessary when apiGateway is called to delete old mappings
+        when(client.getBasePathMappings(any())).thenReturn(
+            new GetBasePathMappingsResult()
+                .withItems(new BasePathMapping().withBasePath("BasePathValue"))
+        );
+        return client;
     }
 
     @Test
-    public void urlUpdater_stage_restApiId() {
+    public void createUpdateRequest_stage_RecordSetWithCorrectName() {
 
-        Optional<ChangeResourceRecordSetsRequest> result = urlUpdater
-            .updateUrlMapping(Stage.TEST, "SomeAPi");
+        assertThat(recordSet.getName(), is(equalTo(staticUrlInfo.getRecordSetName())));
 
-        assertTrue(result.isPresent());
+        String cNameRecord = recordSet.getResourceRecords().get(0).getValue();
+        assertThat(cNameRecord, is(equalTo(domainName)));
+
 
     }
+
+    @Test
+    public void createUpdateRequest_stage_RecordSetWithOneCnameEntry() {
+
+        assertThat(recordSet.getResourceRecords().size(), is(equalTo(1)));
+        assertThat(recordSet.getType(), is(equalTo(RecordType.CNAME.name())));
+    }
+
+    @Test
+    public void createUpdateRequest_stage_RecordSetWithCorrectDomainName() {
+        assertThat(recordSet.getResourceRecords().size(), is(equalTo(1)));
+
+        String cNameRecord = recordSet.getResourceRecords().get(0).getValue();
+        assertThat(cNameRecord, is(equalTo(domainName)));
+    }
+
 
 }
