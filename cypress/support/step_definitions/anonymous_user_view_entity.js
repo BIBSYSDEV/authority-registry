5a9e542d65b33a7aeaa6ebb53d90f6114480d2b9
Eirik Nilsen <eirik.nilsen@unit.no>
2018-12-03 08:36:49 +0100

    Updated testdata

--- cypress/support/step_definitions/anonymous_user_view_entity.js
+++ cypress/support/step_definitions/anonymous_user_view_entity.js
@@ -21,6 +21,18 @@ when('the anonymous user requests the entity', () => {
 
 then("anonymous user can view the entity's data in the native database format", () => {
 	cy.get('@response').then((entityData) => {
-		expect(entityData.body.name).to.be.equal('nameValue')
+		expect(entityData.body.entity.inScheme).to.be.equal('schemeValue')
+		expect(entityData.body.entity.type).to.be.equal('typeValue')
+		expect(entityData.body.entity.identifier).to.be.equal('identifierValue')
+		expect(entityData.body.entity.broader).to.be.equal('broaderValue')
+		expect(entityData.body.entity.perferredLabel[0].lang).to.be.equal('en')
+		expect(entityData.body.entity.perferredLabel[0].value).to.be.equal('preferredLabelValue')
+		expect(entityData.body.entity.alternativeLabel[0].lang).to.be.equal('en')
+		expect(entityData.body.entity.alternativeLabel[0].value).to.be.equal('alternativeLabelValue')
+		expect(entityData.body.entity.narrower).to.be.equal('narrowerValue')
+		expect(entityData.body.entity.related).to.be.equal('relatedValue')
+		expect(entityData.body.entity.definition[0].lang).to.be.equal('en')
+		expect(entityData.body.entity.definition[0].value).to.be.equal('definitionValue')
+		expect(entityData.body.entity.seeAlso).to.be.equal('seeAlsoValue')
 	})
 })
