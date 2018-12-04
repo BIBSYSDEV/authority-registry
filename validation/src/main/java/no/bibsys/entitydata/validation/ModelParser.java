0d9af66e41b841f3078c3d73d7ff47f994348047
Orestis Gkorgkas <og@bibsys.no>
2018-12-04 16:54:45 +0100

    autreg-114: ModelParser lists all URIs that are objects

---
+++ validation/src/main/java/no/bibsys/entitydata/validation/ModelParser.java
@@ -0,0 +1,50 @@
+package no.bibsys.entitydata.validation;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.NodeIterator;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+
+public interface ModelParser {
+
+
+    default Model parseModel(String  jsonString,Lang lang){
+        Model model = ModelFactory.createDefaultModel();
+        InputStream stream = new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8));
+        RDFDataMgr.read(model, stream,lang);
+        return model;
+
+    }
+
+
+    default List<Resource> getObjects(Model model){
+        NodeIterator objects = model.listObjects();
+        List<Resource> result=new ArrayList<>();
+        RDFNode current;
+        while(objects.hasNext()){
+            current= objects.next();
+            if(current.isURIResource()){
+                result.add((Resource)current);
+            }
+
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+}
--- validation/src/test/java/no.bibsys.entitydata.validation/ModelParserTest.java
+++ validation/src/test/java/no.bibsys.entitydata.validation/ModelParserTest.java
@@ -1,25 +1,29 @@
 package no.bibsys.entitydata.validation;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
 import no.bibsys.utils.IoUtils;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.vocabulary.RDF;
 import org.junit.Test;
 
-public class SchemaParserTest {
+public class ModelParserTest implements ModelParser{
 
 
     @Test
     public void parseJson_jsonLdString_model() throws IOException {
         String modelString= IoUtils.resourceAsString(Paths.get("validation","validGraph.json"));
-        SchemaParser schemaParser=new SchemaParser() {};
-        Model actual=schemaParser.parseJsonLD(modelString);
+        Model actual= parseModel(modelString, Lang.JSONLD);
 
         Model expected= ModelFactory.createDefaultModel();
         Resource subject= expected.createResource("http://example.org/a");
@@ -32,7 +36,17 @@ public class SchemaParserTest {
 
         assertTrue(actual.isIsomorphicWith(expected));
 
+    }
 
+
+    @Test
+    public void getObjects_model_allObjectsThatAreIRIsOrLiterals() throws IOException {
+        String modelString=IoUtils.resourceAsString(Paths.get("validation","validationSchema.ttl"));
+        Model model=parseModel(modelString,Lang.TURTLE);
+        List<Resource> objects = getObjects(model);
+
+        int expectedNumberOfObjects=4;
+        assertThat(objects.size(),is(equalTo(expectedNumberOfObjects)));
 
     }
 
