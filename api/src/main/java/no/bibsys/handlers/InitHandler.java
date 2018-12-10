34b679062af28562d2c3685d1a00b65f79c0cadb
Orestis Gkorgkas <og@bibsys.no>
2018-12-10 09:55:07 +0100

    autreg-114: better logging for InitHandler

--- api/src/main/java/no/bibsys/handlers/InitHandler.java
+++ api/src/main/java/no/bibsys/handlers/InitHandler.java
@@ -5,9 +5,13 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.ResourceInUseException;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.route53.model.Change;
 import com.amazonaws.services.route53.model.ChangeResourceRecordSetsRequest;
 import com.amazonaws.services.route53.model.ChangeResourceRecordSetsResult;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import no.bibsys.EnvironmentVariables;
 import no.bibsys.aws.lambda.events.DeployEvent;
 import no.bibsys.aws.lambda.responses.SimpleResponse;
@@ -41,11 +45,10 @@ public class InitHandler extends ResourceHandler {
     @Override
     protected SimpleResponse processInput(DeployEvent input, String apiGatewayQuery,
         Context context) {
-        logger.info("PROCESSING INPUT!");
+
         createApiKeysTable();
         updateUrl();
-
-        return new SimpleResponse("Initializing!!");
+        return new SimpleResponse("Success initializing resources.");
 
     }
 
@@ -61,12 +64,15 @@ public class InitHandler extends ResourceHandler {
 
 
     private void updateUrl() {
-        System.out.println("HELLOOOO");
-        logger.info("Updating url!");
+        logger.debug("Updating URL.");
         UrlUpdater urlUpdater = createUrlUpdater();
 
         Optional<ChangeResourceRecordSetsRequest> request = urlUpdater
             .createUpdateRequest(certificateArn);
+        List<String> changeList= request.map(req -> req.getChangeBatch().getChanges().stream())
+            .orElse(Stream.empty())
+            .map(Change::toString).collect(Collectors.toList());
+        changeList.forEach(change->logger.debug("Change:{}",change));
         ChangeResourceRecordSetsResult result = request
             .map(urlUpdater::executeUpdate)
             .orElseThrow(() -> new NotFoundException("Could not update Static URL settings"));
