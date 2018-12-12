e8532eda1069bf13731fdd49013fb60ba05d562c
Orestis Gkorgkas <og@bibsys.no>
2018-12-12 16:39:23 +0100

    autreg-44: model parser

--- validation/src/main/java/no/bibsys/entitydata/validation/ModelParser.java
+++ validation/src/main/java/no/bibsys/entitydata/validation/ModelParser.java
@@ -17,34 +17,28 @@ import org.apache.jena.riot.RDFDataMgr;
 public interface ModelParser {
 
 
-    default Model parseModel(String  jsonString,Lang lang){
+    default Model parseModel(String jsonString, Lang lang) {
         Model model = ModelFactory.createDefaultModel();
         InputStream stream = new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8));
-        RDFDataMgr.read(model, stream,lang);
+        RDFDataMgr.read(model, stream, lang);
         return model;
 
     }
 
 
-    default List<Resource> getObjects(Model model){
+    default List<Resource> getObjects(Model model) {
         NodeIterator objects = model.listObjects();
-        List<Resource> result=new ArrayList<>();
+        List<Resource> result = new ArrayList<>();
         RDFNode current;
-        while(objects.hasNext()){
-            current= objects.next();
-            if(current.isURIResource()){
-                result.add((Resource)current);
+        while (objects.hasNext()) {
+            current = objects.next();
+            if (current.isURIResource()) {
+                result.add((Resource) current);
             }
 
         }
         return result;
     }
-
-
-
-
-
-
 
 
 }
--- validation/src/test/java/no.bibsys.entitydata.validation/ModelParserTest.java
+++ validation/src/test/java/no.bibsys.entitydata.validation/ModelParserTest.java
@@ -17,22 +17,22 @@ import org.apache.jena.riot.Lang;
 import org.apache.jena.vocabulary.RDF;
 import org.junit.Test;
 
-public class ModelParserTest implements ModelParser{
+public class ModelParserTest implements ModelParser {
 
 
     @Test
     public void parseJson_jsonLdString_model() throws IOException {
-        String modelString= IoUtils.resourceAsString(Paths.get("validation","validGraph.json"));
-        Model actual= parseModel(modelString, Lang.JSONLD);
+        String modelString = IoUtils.resourceAsString(Paths.get("validation", "validGraph.json"));
+        Model actual = parseModel(modelString, Lang.JSONLD);
 
-        Model expected= ModelFactory.createDefaultModel();
-        Resource subject= expected.createResource("http://example.org/a");
-        Resource classObject= expected.createResource("http://example.org/ClassA");
+        Model expected = ModelFactory.createDefaultModel();
+        Resource subject = expected.createResource("http://example.org/a");
+        Resource classObject = expected.createResource("http://example.org/ClassA");
 
-        Property nameProperty= expected.createProperty("http://example.org/name");
+        Property nameProperty = expected.createProperty("http://example.org/name");
 
-        expected.add(expected.createStatement(subject, RDF.type,classObject))
-            .add(expected.createStatement(subject,nameProperty,"entityA"));
+        expected.add(expected.createStatement(subject, RDF.type, classObject))
+            .add(expected.createStatement(subject, nameProperty, "entityA"));
 
         assertTrue(actual.isIsomorphicWith(expected));
 
@@ -41,15 +41,15 @@ public class ModelParserTest implements ModelParser{
 
     @Test
     public void getObjects_model_allObjectsThatAreIRIsOrLiterals() throws IOException {
-        String modelString=IoUtils.resourceAsString(Paths.get("validation","validationSchema.ttl"));
-        Model model=parseModel(modelString,Lang.TURTLE);
+        String modelString = IoUtils
+            .resourceAsString(Paths.get("validation", "validationSchema.ttl"));
+        Model model = parseModel(modelString, Lang.TURTLE);
         List<Resource> objects = getObjects(model);
 
-        int expectedNumberOfObjects=4;
-        assertThat(objects.size(),is(equalTo(expectedNumberOfObjects)));
+        int expectedNumberOfObjects = 4;
+        assertThat(objects.size(), is(equalTo(expectedNumberOfObjects)));
 
     }
-
 
 
 }
