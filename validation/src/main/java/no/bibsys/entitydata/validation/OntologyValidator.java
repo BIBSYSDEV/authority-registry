12c57203b8d990e0a5b05ea59980e6941a2b6e85
Orestis Gkorgkas <og@bibsys.no>
2018-12-12 16:36:37 +0100

    autreg-44: ontology validator

--- validation/src/main/java/no/bibsys/entitydata/validation/OntologyValidator.java
+++ validation/src/main/java/no/bibsys/entitydata/validation/OntologyValidator.java
@@ -1,22 +1,16 @@
 package no.bibsys.entitydata.validation;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
-import java.util.AbstractMap.SimpleEntry;
-import java.util.Collections;
-import java.util.HashMap;
+import java.io.IOException;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
+import no.bibsys.entitydata.validation.rdfutils.ShaclConstants;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.impl.ResourceImpl;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
@@ -25,113 +19,53 @@ import org.slf4j.LoggerFactory;
 
 public class OntologyValidator implements ModelParser {
 
+    public static final String DELIMITER = ",";
     private final static Logger logger = LoggerFactory.getLogger(OntologyValidator.class);
+    private final transient OntologyParser ontologyParser;
+    private final transient ShaclParser shaclParser;
 
-    private final Model ontology;
+    private final transient Map<Resource, Resource> allowedRanges;
 
-    public OntologyValidator(String ontologyString, Lang lang) {
-        this.ontology = parseModel(ontologyString, lang);
+    public OntologyValidator(String ontologyString, Lang ontolgyLang,
+        String shaclModelString, Lang shaclModelLang) {
+        Model ontology = parseModel(ontologyString, ontolgyLang);
+        Model shaclModel = parseModel(shaclModelString, shaclModelLang);
+        this.ontologyParser = new OntologyParser(ontology);
+        this.shaclParser = new ShaclParser(shaclModel);
+        this.allowedRanges = ontologyParser.propertiesWithRange();
     }
+
 
     public Model getOntology() {
-        return ontology;
+        return ontologyParser.getOntology();
     }
 
 
-    public boolean checkModel(Model model) {
-        boolean result = shaclModelTargetClassesAreSubjectsOfOntology(model);
-        if (result) {
+    public boolean checkModel() throws IOException {
 
-            result = allActualPropertiesAreIncludedinOntology(model);
-
-        }
-
-        return result;
+        return shaclModelPropertiesAreIncludedInOntology() &&
+            shaclModelTargetClassesAreClassesOfOntology() &&
+            shaclModelPathObjectsAreOntologyProperties() &&
+            shaclModelDatatypeObjectsMapExactlyPropertyRange() &&
+            shaclModelTargetClassesAreInDomainOfRespectiveProperties();
 
 
     }
 
-    public boolean allActualPropertiesAreIncludedinOntology(Model model) {
+    public boolean shaclModelPropertiesAreIncludedInOntology() {
 
-        Set<Resource> allowedPropeties = listAllowedProperties();
-        Set<Resource> actualPropeties = actualPropertiesOnlyNames(model);
+        Set<Resource> allowedPropeties = ontologyParser.listProperties();
+        Set<Resource> actualPropeties = shaclParser.listPropertyNames();
         actualPropeties.removeAll(allowedPropeties);
         return actualPropeties.isEmpty();
     }
 
 
     @VisibleForTesting
-    public Set<Resource> listAllowedProperties() {
-        return ontology.listResourcesWithProperty(RDF.type, RdfsConstants.PROPERTY_CLASS).toSet();
-    }
-
-
-    public Map<Resource, Resource> actualPropertiesWithRange(Model shaclModel) {
-        Set<Model> propertiesModels = listActualProperties(shaclModel);
-        return propertiesModels.stream().map(this::pathAndDatatype)
-            .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
-
-    }
-
-
-    private Set<Resource> actualPropertiesOnlyNames(Model shaclModel) {
-
-        Set<Model> propertiesModels = listActualProperties(shaclModel);
-        Set<Resource> properties = propertiesModels.stream()
-            .flatMap(model ->
-                model.listObjectsOfProperty(ShaclConstants.PATH).toSet().stream())
-            .map(rdfNode -> (Resource) rdfNode).collect(Collectors.toSet());
-
-        return properties;
-    }
-
-    public Map<Resource, RDFNode> allowedPropertiesWithRange() {
-        return ontology
-            .listResourcesWithProperty(RDF.type, RdfsConstants.PROPERTY_CLASS)
-            .toList().stream()
-            .flatMap(resource -> ontology.listStatements(resource, RDFS.range, null, null)
-                .toList().stream())
-            .map(this::subjectAndObjectFromStatement)
-            .collect(Collectors.toMap(SimpleEntry::getKey, SimpleEntry::getValue));
-
-
-    }
-
-    private Entry<Resource, Resource> pathAndDatatype(Model blankNodeModel) {
-        List<RDFNode> propertyNameList = blankNodeModel
-            .listObjectsOfProperty(ShaclConstants.PATH)
-            .toList();
-        Preconditions.checkArgument(propertyNameList.size() == 1,
-            "Only one sh:path object is allowed per property model");
-        List<RDFNode> propertyRangeList = blankNodeModel
-            .listObjectsOfProperty(ShaclConstants.DATATYPE).toList();
-        Preconditions.checkArgument(propertyRangeList.size() == 1,
-            "Only one sh:datatype object is allowed per property model");
-        Resource propertyName = (Resource) propertyNameList.iterator().next();
-        Resource propertyRange = (Resource) propertyRangeList.iterator().next();
-        return new HashMap.SimpleEntry<>(propertyName, propertyRange);
-
-
-    }
-
-    private SimpleEntry<Resource, Resource> subjectAndObjectFromStatement(Statement statement) {
-        Resource subject = statement.getSubject();
-        Resource object = (Resource) statement.getObject();
-        return new HashMap.SimpleEntry<Resource, Resource>(subject, object);
-
-    }
-
-    private Set<Model> listActualProperties(Model model) {
-        List<RDFNode> properties = model.listObjectsOfProperty(ShaclConstants.PROPERTY)
-            .toList();
-        return properties.stream().map(rdfNode -> (ResourceImpl) rdfNode)
-            .map(resource -> resource.listProperties().toModel()).collect(Collectors.toSet());
-
-    }
-
-    private boolean shaclModelTargetClassesAreSubjectsOfOntology(Model model) {
-        Set<Resource> subjects = ontology.listSubjects().toSet();
-        Set<Resource> objects = resourceObjectNodes(model, ShaclConstants.TARGETCLASS_PROPERTY);
+    public boolean shaclModelTargetClassesAreClassesOfOntology() {
+        Set<Resource> subjects = ontologyParser.listSubjects(RDF.type, RDFS.Class);
+        Set<Resource> objects = shaclParser
+            .resourceObjectNodes(ShaclConstants.TARGETCLASS_PROPERTY);
         return isSubset(subjects, objects);
 
 
@@ -145,7 +79,7 @@ public class OntologyValidator implements ModelParser {
             if (superSet.containsAll(subset)) {
                 return true;
             } else {
-                logger.warn("Invalid resources:{}", informOnWrongResource(superSet, subset));
+                logger.warn("Invalid resources:{}", showSetDiffForErrorMessage(superSet, subset));
                 return false;
             }
 
@@ -153,11 +87,11 @@ public class OntologyValidator implements ModelParser {
 
     }
 
-    private String informOnWrongResource(Set<Resource> superSet, Set<Resource> subset) {
+    private String showSetDiffForErrorMessage(Set<Resource> superSet, Set<Resource> subset) {
         Set<Resource> diff = calculateDiff(superSet, subset);
         Set<String> diffStrings = diff.stream().map(resource -> resource.toString()).collect(
             Collectors.toSet());
-        String wrongResources = String.join(",", diffStrings);
+        String wrongResources = String.join(DELIMITER, diffStrings);
         return wrongResources;
 
 
@@ -170,25 +104,48 @@ public class OntologyValidator implements ModelParser {
         return diff;
     }
 
-    private Set<Resource> resourceObjectNodes(Model model, Property targetClassProperty) {
-        List<RDFNode> objectNodes = model
-            .listObjectsOfProperty(targetClassProperty).toList();
-        if (areRDFNodesResources(objectNodes)) {
-            return objectNodes.stream().map(rdfNode -> (Resource) rdfNode)
-                .collect(Collectors.toSet());
+
+    public boolean shaclModelPathObjectsAreOntologyProperties() {
+        Set<Resource> allowedProperties = ontologyParser.listProperties();
+        Set<Resource> actualProperties = shaclParser.listPropertyNames();
+        return allowedProperties.containsAll(actualProperties);
+
+
+    }
+
+    public boolean shaclModelDatatypeObjectsMapExactlyPropertyRange() {
+
+        Map<Resource, Resource> actualRanges = shaclParser.propertiesWithRange();
+
+        Set<Entry<Resource, Resource>> validationSet = actualRanges.entrySet().stream()
+            .filter(this::isNotValidRange)
+            .collect(Collectors.toSet());
+
+        return validationSet.isEmpty();
+
+
+    }
+
+
+    private boolean isNotValidRange(Entry<Resource, Resource> propertyRangeEntry) {
+        Resource propertyUri = propertyRangeEntry.getKey();
+        if (allowedRanges.containsKey(propertyUri)) {
+            Resource actualRange = propertyRangeEntry.getValue();
+            Resource allowedRange = allowedRanges.get(propertyUri);
+            return !allowedRange.equals(actualRange);
         } else {
-            return Collections.emptySet();
+            return true;
         }
-    }
 
-    private boolean areRDFNodesResources(List<RDFNode> rdfNodes) {
-        for (RDFNode node : rdfNodes) {
-            if (!node.isResource()) {
-                return false;
-            }
-        }
-        return true;
     }
 
 
+    public boolean shaclModelTargetClassesAreInDomainOfRespectiveProperties() throws IOException {
+        Model ontologyDomains = ontologyParser.getOntology()
+            .listStatements(null, RDFS.domain, (RDFNode) null).toModel();
+
+        Model shaclDomains = shaclParser.generateDomainModel();
+
+        return ontologyDomains.containsAll(shaclDomains);
+    }
 }
--- validation/src/test/java/no.bibsys.entitydata.validation/OntologyValidatorTest.java
+++ validation/src/test/java/no.bibsys.entitydata.validation/OntologyValidatorTest.java
@@ -1,92 +1,128 @@
 package no.bibsys.entitydata.validation;
 
 
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Set;
-
 import no.bibsys.utils.IoUtils;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.riot.Lang;
 import org.junit.Test;
 
 public class OntologyValidatorTest implements ModelParser {
 
 
-    private final transient OntologyValidator ontologyValidator;
-
-
-    public OntologyValidatorTest() throws IOException {
-        ontologyValidator = initializeOntologyValidator();
-    }
-
+    public static final String RESOURCES_PATH = "validation";
+    public static final String ENTITY_ONTOLOGY_TTL = "unit-entity-ontology.ttl";
+    private static final String VALID_SCHEMA = "validShaclValidationSchema.ttl";
+    private static final String INVALID_PATH_SCHEMA = "invalidPathObjectShaclValidationSchema.ttl";
+    private static final String INVALID_CLASS_SCHEMA = "invalidClassShaclValidationSchema.ttl";
+    private static final String INVALID_DATATYPE_SCHEMA = "invalidDatatypeRangeShaclValidationSchema.ttl";
+    private static final String INVALID_DOMAIN_SCEMA = "invalidPropertyDomainShaclValidationSchema.ttl";
 
     @Test
-    public void loadOntology_ontologyString_OntologyModel() {
+    public void loadOntology_ontologyString_OntologyModel() throws IOException {
+        OntologyValidator ontologyValidator = initializeOntologyValidator(VALID_SCHEMA);
         Model model = ontologyValidator.getOntology();
         assertFalse(model.isEmpty());
     }
 
 
     @Test
-    public void checkModel_shaclValidationSchema_validIfItContainsOnlyObjectsDefinedInOntology()
+    public void shaclModelTargetClassesAreClassesOfOntology_validShaclSchema_valid()
         throws IOException {
 
-        OntologyValidator validator = initializeOntologyValidator();
-        String validdationModelString = IoUtils
-            .resourceAsString(Paths.get("validation", "validationSchema.ttl"));
-        Model schaclModel = parseModel(validdationModelString, Lang.TURTLE);
-        boolean result = validator.checkModel(schaclModel);
-
+        OntologyValidator validator = initializeOntologyValidator(VALID_SCHEMA);
+        boolean result = validator.shaclModelTargetClassesAreClassesOfOntology();
         assertTrue(result);
+        assertTrue(validator.checkModel());
+
 
     }
 
 
     @Test
-    public void listAllowedProperties_void_completeListWithPropertiesInOntology()
+    public void shaclModelTargetClassesAreClassesOfOntology_invalidShackValidationSchema_notValid()
         throws IOException {
-        OntologyValidator validator = initializeOntologyValidator();
-        Set<Resource> properties = validator
-            .listAllowedProperties();
-        assertThat(properties.size(), is(equalTo(9)));
+
+        OntologyValidator validator = initializeOntologyValidator(INVALID_CLASS_SCHEMA);
+        boolean result = validator.shaclModelTargetClassesAreClassesOfOntology();
+        assertFalse(result);
+        assertFalse(validator.checkModel());
+
     }
 
 
     @Test
-    public void listActualProperties_void_listOfPropertiesInShaclModel()
+    public void shaclModelPathObjectsAreOntologyProperties_validShaclShchema_valid()
         throws IOException {
-        OntologyValidator validator = initializeOntologyValidator();
-        Model shaclModel = validationSchema();
-        List<Model> properties = validator.listActualProperties(shaclModel);
+        OntologyValidator validator = initializeOntologyValidator(VALID_SCHEMA);
+        assertTrue(validator.shaclModelPathObjectsAreOntologyProperties());
+        assertTrue(validator.checkModel());
 
-        assertThat(properties.size(), is(equalTo(1)));
+
+    }
+
+    @Test
+    public void shaclModelPathObjectsAreOntologyProperties_invalidShaclShchema_notValid()
+        throws IOException {
+        OntologyValidator validator = initializeOntologyValidator(INVALID_PATH_SCHEMA);
+        assertFalse(validator.shaclModelPathObjectsAreOntologyProperties());
+        assertFalse(validator.checkModel());
+
     }
 
 
-    private OntologyValidator initializeOntologyValidator() throws IOException {
+    @Test
+    public void shaclModelDatatypeObjectsMapExactlyPropertyRange_validShackSchema_valid()
+        throws IOException {
+        OntologyValidator validator = initializeOntologyValidator(VALID_SCHEMA);
+        assertTrue(validator.shaclModelDatatypeObjectsMapExactlyPropertyRange());
+        assertTrue(validator.checkModel());
+
+    }
+
+    @Test
+    public void shaclModelDatatypeObjectsMapExactlyPropertyRange_invalidDatatypeRangeShaclSchema_notValid()
+        throws IOException {
+        OntologyValidator validator = initializeOntologyValidator(INVALID_DATATYPE_SCHEMA);
+        assertFalse(validator.shaclModelDatatypeObjectsMapExactlyPropertyRange());
+        assertFalse(validator.checkModel());
+
+    }
+
+
+    @Test
+    public void shaclModelTargetClassesAreInDomainOfRespectiveProperties_validShaclSchema_valid()
+        throws IOException {
+        OntologyValidator validator = initializeOntologyValidator(VALID_SCHEMA);
+        assertTrue(validator.shaclModelTargetClassesAreInDomainOfRespectiveProperties());
+        assertTrue(validator.checkModel());
+
+    }
+
+
+    @Test
+    public void shaclModelTargetClassesAreInDomainOfRespectiveProperties_invalidShaclSchema_valid()
+        throws IOException {
+        OntologyValidator validator = initializeOntologyValidator(INVALID_DOMAIN_SCEMA);
+        assertFalse(validator.shaclModelTargetClassesAreInDomainOfRespectiveProperties());
+        assertFalse(validator.checkModel());
+
+    }
+
+
+    private OntologyValidator initializeOntologyValidator(String validationSchemeFilename)
+        throws IOException {
         String ontologyString = IoUtils.resourceAsString(
-            Paths.get("validation", "unit-entity-ontology.ttl"));
+            Paths.get(RESOURCES_PATH, ENTITY_ONTOLOGY_TTL));
+        String shaclModelString = IoUtils
+            .resourceAsString(Paths.get(RESOURCES_PATH, validationSchemeFilename));
 
-        return new OntologyValidator(ontologyString, Lang.TURTLE);
+        return new OntologyValidator(ontologyString, Lang.TURTLE, shaclModelString, Lang.TURTLE);
 
-
-    }
-
-
-    private Model validationSchema() throws IOException {
-        String validdationModelString = IoUtils
-            .resourceAsString(Paths.get("validation", "validationSchema.ttl"));
-        Model schaclModel = parseModel(validdationModelString, Lang.TURTLE);
-        return schaclModel;
     }
 
 
