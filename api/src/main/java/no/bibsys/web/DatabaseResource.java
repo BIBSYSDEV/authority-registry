d7c44e88a974d47d637bda18135579c938ab61b7
Orestis Gkorgkas <og@bibsys.no>
2019-01-10 19:57:20 +0100

    new DatabaseRousourceTests and removal of empty function

--- api/src/main/java/no/bibsys/web/DatabaseResource.java
+++ api/src/main/java/no/bibsys/web/DatabaseResource.java
@@ -1,10 +1,28 @@
 package no.bibsys.web;
 
+import com.amazonaws.services.s3.Headers;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.extensions.Extension;
+import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -24,27 +42,6 @@ import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
-
-import com.amazonaws.services.s3.Headers;
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.extensions.Extension;
-import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import no.bibsys.service.EntityService;
 import no.bibsys.service.RegistryService;
 import no.bibsys.web.model.EntityDto;
@@ -89,8 +86,8 @@ public class DatabaseResource {
                     value = AwsApiGatewayIntegration.AWS_PROXY), }) })
     @SecurityRequirement(name = ApiKeyConstants.API_KEY)
     @RolesAllowed({ Roles.API_ADMIN })
-    public Response createRegistry(@HeaderParam(ApiKeyConstants.API_KEY_PARAM_NAME) String apiKey,
-            @RequestBody(description = "Request object to create registry",
+    public Response createRegistry(
+        @RequestBody(description = "Request object to create registry",
                     content = @Content(schema = @Schema(
                             implementation = RegistryDto.class))) RegistryDto registryDto)
             throws Exception {
@@ -247,29 +244,6 @@ public class DatabaseResource {
         return Response.ok(newApiKey).build();
     }
     
-    @DELETE
-    @Path("/{registryName}/empty")
-    @Operation(extensions = { @Extension(name = AwsApiGatewayIntegration.INTEGRATION, properties = {
-            @ExtensionProperty(name = AwsApiGatewayIntegration.URI,
-                    value = AwsApiGatewayIntegration.URI_OBJECT, parseValue = true),
-            @ExtensionProperty(name = AwsApiGatewayIntegration.REQUEST_PARAMETERS,
-                    value = AwsApiGatewayIntegration.REQUEST_PARAMETERS_OBJECT, parseValue = true),
-            @ExtensionProperty(name = AwsApiGatewayIntegration.PASSTHROUGH_BEHAVIOR,
-                    value = AwsApiGatewayIntegration.WHEN_NO_MATCH),
-            @ExtensionProperty(name = AwsApiGatewayIntegration.HTTPMETHOD, value = HttpMethod.POST),
-            @ExtensionProperty(name = AwsApiGatewayIntegration.TYPE,
-                    value = AwsApiGatewayIntegration.AWS_PROXY), }) })
-    @SecurityRequirement(name = ApiKeyConstants.API_KEY)
-    @RolesAllowed({ Roles.API_ADMIN, Roles.REGISTRY_ADMIN })
-    public Response emptyRegistry(@HeaderParam(ApiKeyConstants.API_KEY_PARAM_NAME) String apiKey,
-            @Parameter(in = ParameterIn.PATH, name = REGISTRY_NAME, required = true,
-                    description = "Name of registry to delete", schema = @Schema(
-                            type = STRING)) @PathParam(REGISTRY_NAME) String registryName) {
-
-
-        registryService.emptyRegistry(registryName);
-        return Response.ok(String.format("Registry %s has been emptied", registryName)).build();
-    }
 
     @GET
     @Path("/{registryName}/schema")
@@ -328,6 +302,7 @@ public class DatabaseResource {
 
     @POST
     @Path("/{registryName}/entity")
+
     @Operation(extensions = { @Extension(name = AwsApiGatewayIntegration.INTEGRATION, properties = {
             @ExtensionProperty(name = AwsApiGatewayIntegration.URI,
                     value = AwsApiGatewayIntegration.URI_OBJECT, parseValue = true),
@@ -399,8 +374,6 @@ public class DatabaseResource {
                     content = @Content(array = @ArraySchema(schema = @Schema(implementation = EntityDto.class)))) EntityDto... entityDtos)
             throws IOException {
 
-        
-        
         List<EntityDto> persistedEntities = new ArrayList<EntityDto>();
         Arrays.asList(entityDtos).forEach(entityDto -> {
             EntityDto persistedEntity = entityService.addEntity(registryName, entityDto);
