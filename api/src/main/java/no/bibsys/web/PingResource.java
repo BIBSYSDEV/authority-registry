6983869c31e5e639ff2ff266489c63a026530aa5
Orestis Gkorgkas <og@bibsys.no>
2019-01-10 12:59:43 +0100

    check if headers go through AWS

--- api/src/main/java/no/bibsys/web/PingResource.java
+++ api/src/main/java/no/bibsys/web/PingResource.java
@@ -1,17 +1,21 @@
 package no.bibsys.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.extensions.Extension;
+import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.extensions.Extension;
-import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
 import no.bibsys.web.security.ApiKeyConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/ping")
 public class PingResource {
+
+    private static final Logger logger = LoggerFactory.getLogger(PingResource.class);
 
     @GET
     @Path("/")
@@ -26,6 +30,12 @@ public class PingResource {
             @ExtensionProperty(name = AwsApiGatewayIntegration.TYPE,
                     value = AwsApiGatewayIntegration.AWS_PROXY), }) })
     public Response ping(@HeaderParam(ApiKeyConstants.API_KEY_PARAM_NAME) String apiKey) {
+        if (apiKey == null) {
+            System.out.println("APIKEY is null");
+            logger.info("APIKEY is null");
+        }
+        System.out.println(apiKey);
+        logger.info("APIKEY:" + apiKey);
         return Response.ok().build();
     }
 }
