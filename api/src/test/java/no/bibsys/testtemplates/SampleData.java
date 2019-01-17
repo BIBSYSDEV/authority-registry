509d31a6081d8d564c5751fb2e82cfd8e08f2350
Orestis Gkorgkas <og@bibsys.no>
2019-01-17 14:39:16 +0100

    AUTREG-151: removing imports

--- api/src/test/java/no/bibsys/testtemplates/SampleData.java
+++ api/src/test/java/no/bibsys/testtemplates/SampleData.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.io.Serializable;
 import java.util.Map;
 import no.bibsys.web.model.EntityDto;
 import no.bibsys.web.model.RegistryDto;
