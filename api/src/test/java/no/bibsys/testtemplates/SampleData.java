52f1ec102b39966db838858a349c9ce331dfde09
en <en@brucia.bibsys.no>
2019-01-21 07:50:57 +0100

    fixed html template for use with json-ld

--- api/src/test/java/no/bibsys/testtemplates/SampleData.java
+++ api/src/test/java/no/bibsys/testtemplates/SampleData.java
@@ -1,10 +1,12 @@
 package no.bibsys.testtemplates;
 
+import java.util.Map;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.util.Map;
+
 import no.bibsys.web.model.EntityDto;
 import no.bibsys.web.model.RegistryDto;
 
@@ -20,20 +22,22 @@ public class SampleData {
         ObjectNode body = mapper.createObjectNode();
         body.put("label", "A random label");
         body.put("number", 5);
+        body.put("@id","http://example.org/fakevoc/c00000");
+        body.put("@type", "bsa:Concept");
         ArrayNode array = body.putArray("myArray");
         array.add(1);
         array.add(2);
         array.add(3);
         ObjectNode langString = body.putObject("langString");
-        langString.put("lang", "en");
-        langString.put("value", "langStringValue");
+        langString.put("@language", "en");
+        langString.put("@value", "langStringValue");
         ArrayNode langArray = body.putArray("myLangArray");
         ObjectNode langArrayString1 = langArray.addObject();
-        langArrayString1.put("lang", "en");
-        langArrayString1.put("value", "langStringValue1");
+        langArrayString1.put("@language", "en");
+        langArrayString1.put("@value", "langStringValue1");
         ObjectNode langArrayString2 = langArray.addObject();
-        langArrayString2.put("lang", "no");
-        langArrayString2.put("value", "langStringValue2");
+        langArrayString2.put("@language", "no");
+        langArrayString2.put("@value", "langStringValue2");
         
         EntityDto entityDto = new EntityDto();
 
--- api/src/test/java/no/bibsys/web/DatabaseResourceTest.java
+++ api/src/test/java/no/bibsys/web/DatabaseResourceTest.java
@@ -7,21 +7,29 @@ import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertThat;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.s3.Headers;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.s3.Headers;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import no.bibsys.JerseyConfig;
 import no.bibsys.LocalDynamoDBHelper;
 import no.bibsys.MockEnvironment;
@@ -33,10 +41,6 @@ import no.bibsys.testtemplates.SampleData;
 import no.bibsys.web.model.EntityDto;
 import no.bibsys.web.model.RegistryDto;
 import no.bibsys.web.security.ApiKeyConstants;
-import org.glassfish.jersey.test.JerseyTest;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 
 public class DatabaseResourceTest extends JerseyTest {
@@ -462,8 +466,12 @@ public class DatabaseResourceTest extends JerseyTest {
         createRegistry(registryName, apiAdminKey);
         EntityDto entity = createEntity(registryName).readEntity(EntityDto.class);
 
+        System.out.println(entity.getBody());
+        
         Response entityAsHtml = readEntity(registryName, entity.getId(), MediaType.TEXT_HTML);
         String html = entityAsHtml.readEntity(String.class);
+        
+        System.out.println(html);
 
         assertThat(html.toLowerCase(), containsString("html"));
         assertThat(html.toLowerCase(), containsString("data-automation-id=\"label\""));
