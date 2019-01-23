70461a6af2ac71701a2f4ed09020b4ef03cef5bc
Orestis <orestis.gorgas@gmail.com>
2019-01-23 22:27:44 +0100

    AUTREG-127: small fixes

--- dynamo/src/test/java/no/bibsys/db/LocalDynamoTest.java
+++ dynamo/src/test/java/no/bibsys/db/LocalDynamoTest.java
@@ -1,12 +1,14 @@
 package no.bibsys.db;
 
+import java.io.IOException;
 import org.junit.Before;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public abstract class LocalDynamoTest extends DynamoTest {
-
 
     protected AmazonDynamoDB localClient;
     protected RegistryManager registryManager;
@@ -14,7 +16,7 @@ public abstract class LocalDynamoTest extends DynamoTest {
     protected SampleData sampleData;
 
     @Before
-    public void init() {
+    public void init() throws IOException {
         System.setProperty("sqlite4java.library.path", "build/libs");
         System.setProperty("java.library.path", "native-libs");
         System.setProperty("sqlite4java.library.path", "build/libs");
@@ -22,8 +24,9 @@ public abstract class LocalDynamoTest extends DynamoTest {
         localClient = DynamoDBEmbedded.create().amazonDynamoDB();
         registryManager = new RegistryManager(localClient);
         entityManager = new EntityManager(localClient);
-        
+
         sampleData = new SampleData();
+
     }
 
     protected TableDriver newTableDriver() {
