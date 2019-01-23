7d8e1128fd37ff03e9c66d97181deb95ba9fd75b
Orestis <orestis.gorgas@gmail.com>
2019-01-23 22:26:34 +0100

    AUTREG-127: one more exception

--- validation/src/main/java/no/bibsys/entitydata/validation/exceptions/ValidationSchemaSyntaxErrorException.java
+++ validation/src/main/java/no/bibsys/entitydata/validation/exceptions/ValidationSchemaSyntaxErrorException.java
@@ -4,6 +4,9 @@ import org.apache.jena.riot.RiotException;
 
 public class ValidationSchemaSyntaxErrorException extends RiotException {
 
+    private static final long serialVersionUID = -2866223016304119200L;
+
+
     public ValidationSchemaSyntaxErrorException(RiotException e) {
         super(e);
     }
