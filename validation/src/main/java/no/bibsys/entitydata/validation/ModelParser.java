672990bb2ccdd29d7f95b688980e4f41b80ac48a
Orestis Gkorgkas <og@bibsys.no>
2019-01-25 15:56:21 +0100

    AUTREG-127:add @JsonIgnore labels
    Avoid methods being serialized in classes that extend the ModelParser

--- validation/src/main/java/no/bibsys/entitydata/validation/ModelParser.java
+++ validation/src/main/java/no/bibsys/entitydata/validation/ModelParser.java
@@ -1,5 +1,6 @@
 package no.bibsys.entitydata.validation;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.StringWriter;
@@ -17,28 +18,26 @@ import org.apache.jena.riot.RiotException;
 
 public class ModelParser {
 
+    @JsonIgnore
     public Model parseModel(String dataString, Lang lang) {
         try {
             Model model = ModelFactory.createDefaultModel();
             InputStream stream = new ByteArrayInputStream(dataString.getBytes(StandardCharsets.UTF_8));
             RDFDataMgr.read(model, stream, lang);
             return model;
-        }
-        catch(RiotException e){
+        } catch(RiotException e){
             throw new ValidationSchemaSyntaxErrorException(e);
         }
     }
 
+    @JsonIgnore
     public Set<Resource> getUriResourceObjects(Model model) {
-        return model.listObjects()
-            .toSet().stream()
-            .filter(RDFNode::isURIResource)
-            .map(rdfNode -> (Resource) rdfNode)
+        return model.listObjects().toSet().stream().filter(RDFNode::isURIResource).map(rdfNode -> (Resource) rdfNode)
             .collect(Collectors.toSet());
     }
 
 
-
+    @JsonIgnore
     public String writeData(Model model, Lang lang){
         StringWriter writer=new StringWriter();
         RDFDataMgr.write(writer, model, lang);
