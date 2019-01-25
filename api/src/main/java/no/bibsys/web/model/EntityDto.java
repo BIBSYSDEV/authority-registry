e9a665fb7b933d92f43fa7527aad948663967c1a
Orestis Gkorgkas <og@bibsys.no>
2019-01-25 15:45:59 +0100

    AUTREG-127: add isomorphic comparison to EntityDto. Remove getBodyAsJson

--- api/src/main/java/no/bibsys/web/model/EntityDto.java
+++ api/src/main/java/no/bibsys/web/model/EntityDto.java
@@ -2,8 +2,6 @@ package no.bibsys.web.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRawValue;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -11,15 +9,92 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 import javax.xml.bind.DatatypeConverter;
 import javax.xml.bind.annotation.XmlRootElement;
+import no.bibsys.entitydata.validation.ModelParser;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
 
 @XmlRootElement
-public class EntityDto {
+public class EntityDto extends ModelParser {
 
     private String id;
     private String created;
     private String modified;
     private String path;
     private String body;
+
+    @JsonIgnore
+    public String getEtagValue() {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            byte[] hash = digest.digest(getBody().getBytes(StandardCharsets.UTF_8));
+            String hex = DatatypeConverter.printHexBinary(hash);
+            return hex;
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+
+    }
+
+
+    @JsonIgnore
+    public boolean isIsomorphic(EntityDto other) {
+        if(this.body==null && other.getBody()==null){
+            return true;
+        }
+        else if(this.body!=null && other.getBody()!=null){
+            Model thisModel = parseModel(getBody(), Lang.JSONLD);
+            Model thatModel = parseModel(other.getBody(), Lang.JSONLD);
+            return thisModel.isIsomorphicWith(thatModel);
+        }
+        else {
+            // one null and the other is not
+            return false;
+        }
+
+    }
+
+    @JsonRawValue
+    @JsonDeserialize(using = JsonAsStringDeserializer.class)
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void validate() {
+        // TODO: validate body
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getCreated() != null ? getCreated().hashCode() : 0);
+        result = 31 * result + (getModified() != null ? getModified().hashCode() : 0);
+        result = 31 * result + (getPath() != null ? getPath().hashCode() : 0);
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof EntityDto)) {
+            return false;
+        }
+
+        EntityDto other = (EntityDto) o;
+        return Objects.equals(id, other.getId()) &&
+            Objects.equals(created, other.getCreated()) &&
+            Objects.equals(modified, other.getModified()) &&
+            Objects.equals(path, other.getPath()) && (
+             isIsomorphic(other)
+        );
+
+    }
 
     public String getId() {
         return id;
@@ -44,25 +119,9 @@ public class EntityDto {
     public void setModified(String modified) {
         this.modified = modified;
     }
-    
-    @JsonRawValue
-    @JsonDeserialize(using = JsonAsStringDeserializer.class)
-    public String getBody() {
-        return body;
-    }
-    
-    @JsonIgnore
-    public String getBodyAsJson() throws JsonProcessingException {
-        return new ObjectMapper().writeValueAsString(body);
-    }
 
-    public void setBody(String body) {
-        this.body = body;
-    }
-    
     /**
      * Relative path to this resource, set in the API level
-     * @return
      */
     public String getPath() {
         return path;
@@ -71,50 +130,11 @@ public class EntityDto {
     public void setPath(String path) {
         this.path = path;
     }
-    
-    @JsonIgnore
-    public String getEtagValue() {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("MD5");
-            byte[] hash = digest.digest(getBodyAsJson().getBytes(StandardCharsets.UTF_8));
-            String hex = DatatypeConverter.printHexBinary(hash);
-            return hex;            
-        } catch (NoSuchAlgorithmException | JsonProcessingException e) {
-            return null;
-        }
-
-    }
-
-
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(body);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (!(obj instanceof EntityDto)) {
-            return false;
-        }
-        EntityDto other = (EntityDto) obj;
-        return Objects.equals(body, other.body);
-    }
 
     @Override
     public String toString() {
         return "EntityDto [id=" + id + ", created=" + created + ", modified=" + modified + ", body="
-                + body + "]";
+            + body + "]";
     }
 
-    public void validate() {
-        // TODO: validate body
-    }
-    
 }
