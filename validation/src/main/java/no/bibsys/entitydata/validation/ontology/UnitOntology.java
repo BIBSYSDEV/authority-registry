711e55f21a1544464c31c361288d74eab3ec8301
Orestis Gkorgkas <og@bibsys.no>
2019-01-30 13:20:38 +0100

    AUTREF-127: add unit:Concept resource

--- validation/src/main/java/no/bibsys/entitydata/validation/ontology/UnitOntology.java
+++ validation/src/main/java/no/bibsys/entitydata/validation/ontology/UnitOntology.java
@@ -1,7 +1,9 @@
 package no.bibsys.entitydata.validation.ontology;
 
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.impl.PropertyImpl;
+import org.apache.jena.rdf.model.impl.ResourceImpl;
 
 public class UnitOntology {
 
@@ -9,5 +11,7 @@ public class UnitOntology {
     public static String  NAMESPACE="http://unit.no/entitydata#";
     private static final String  preferredLabelString= String.join("",NAMESPACE,"preferredLabel");
     public static final Property PREFERRED_LABEL= new PropertyImpl(preferredLabelString);
+    private static final String conceptString = String.join("", NAMESPACE, "Concept");
+    public static final Resource CONCEPT = new ResourceImpl(conceptString);
 
 }
