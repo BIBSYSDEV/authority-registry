30f56b7ac7e313a005c9c25cb69961617efe2805
Orestis Gkorgkas <og@bibsys.no>
2019-01-30 13:20:57 +0100

    AUTREF-127: read model for extracting values

--- api/src/main/java/no/bibsys/web/model/EntityHtmlMessageBodyWriter.java
+++ api/src/main/java/no/bibsys/web/model/EntityHtmlMessageBodyWriter.java
@@ -9,9 +9,7 @@ import java.io.PrintWriter;
 import java.io.Writer;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import javax.ws.rs.Produces;
@@ -22,6 +20,11 @@ import javax.ws.rs.ext.Provider;
 import no.bibsys.aws.tools.JsonUtils;
 import no.bibsys.entitydata.validation.ModelParser;
 import no.bibsys.entitydata.validation.ontology.UnitOntology;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.vocabulary.RDF;
 
 @Provider
 @Produces(MediaType.TEXT_HTML)
@@ -29,14 +32,13 @@ public class EntityHtmlMessageBodyWriter extends ModelParser implements MessageB
 
     private static final String NO_LABEL = "(No label)";
 
-    private static final String VALUE = JsonLdConsts.VALUE;
-    private static final String LANG = JsonLdConsts.LANGUAGE;
     private static final String LANG_NO = "no";
     private static final String LANG_EN = "en";
     private static final String ID = "id";
     private static final String BODY = "body";
     private static final String ENTITY_TEMPLATE = "entitytemplate";
     private static final String LABEL = "label";
+    public static final Lang SUPPORTED_LANGUAGE = Lang.JSONLD;
 
 
     @Override
@@ -66,30 +68,34 @@ public class EntityHtmlMessageBodyWriter extends ModelParser implements MessageB
         Map<String, Object> entityMap = new ConcurrentHashMap<>();
 
         Map<?, ?> bodyMap = JsonUtils.newJsonParser().readValue(entity.getBody(), Map.class);
+        Model model = parseModel(entity.getBody(), SUPPORTED_LANGUAGE);
+
         bodyMap.remove(JsonLdConsts.CONTEXT);
         entityMap.put(BODY, bodyMap);
-        entityMap.put(ID, entity.getId());
-        List<?> preferredLabel = (List<?>) bodyMap.get(UnitOntology.PREFERRED_LABEL);
-        String label = findTitle(preferredLabel);
-        entityMap.put(LABEL, label);
+        entityMap.put(ID, extractId(model));
+        entityMap.put(LABEL, extractTitleFromPreferredLabel(model));
 
         return entityMap;
     }
 
-    private String findTitle(List<?> preferredLabel) {
+    private String extractId(Model model) {
+        Resource resource = model.listSubjectsWithProperty(RDF.type, UnitOntology.CONCEPT).nextResource();
+        return resource.toString();
+    }
+
+    private String extractTitleFromPreferredLabel(Model model) {
         String label = NO_LABEL;
-        if (Objects.nonNull(preferredLabel) && !preferredLabel.isEmpty()) {
-            @SuppressWarnings("unchecked") Map<String, String> titleMap = preferredLabel.stream().filter(
-                labelObject -> ((Map<String, String>) labelObject).get(LANG).equals(LANG_EN)
-                    || ((Map<String, String>) labelObject).get(LANG).equals(LANG_NO)).collect(Collectors
-                .toMap(labelObject -> ((Map<String, String>) labelObject).get(LANG),
-                    labelObject -> ((Map<String, String>) labelObject).get(VALUE)));
-            if (titleMap.containsKey(LANG_NO)) {
-                label = titleMap.get(LANG_NO);
-            } else if (titleMap.containsKey(LANG_EN)) {
-                label = titleMap.get(LANG_EN);
-            }
+        Map<String, String> labels = model.listObjectsOfProperty(UnitOntology.PREFERRED_LABEL)
+            .filterKeep(rdfNode -> rdfNode.isLiteral()).mapWith(rdfNode -> rdfNode.asLiteral()).toList().stream()
+            .collect(Collectors.toMap(Literal::getLanguage, lit -> lit.getValue().toString()));
+        if (labels.containsKey(LANG_NO)) {
+            label = labels.get(LANG_NO);
+        } else if (labels.containsKey(LANG_EN)) {
+            label = labels.get(LANG_EN);
+        } else if (!label.isEmpty()) {
+            label = labels.values().iterator().next();
         }
         return label;
+
     }
 }
