acdbc5c915a7fd281cfec8ca0cd3ff2c01ffc731
en <en@brucia.bibsys.no>
2019-01-30 13:25:12 +0100

    Codacy is complaining

--- cypress/support/step_definitions/anonymous_user_view_entity_registry_metadata_HTML_RDF.js
+++ cypress/support/step_definitions/anonymous_user_view_entity_registry_metadata_HTML_RDF.js
@@ -23,25 +23,25 @@ When('an anonymous user dereferences the base URI for the registry specifying me
   cy.wrap(formats).as('formats');
 });
 
-//Scenario: An anonymous user views the metadata for a registry as HTML
-//Given that there is an existing populated entity registry with a schema
-//When an anonymous user dereferences the base URI for the registry specifying mediatype text/html
-//Then they see metadata related to the entity registry regarding:
-//| Registry name                    |
-//| Registry type                    |
-//| Publisher                        |
-//| License for the data             |
-//| Owner organisation               |
-//| Participating organisations      |
-//| Languages used in dataset        |
-//| Creation date                    |
-//| Modification date                |
-//| Relations to other data sets     |
-//| Location of APIs                 |
-//| Example resources                |
-//| Base URI for dataset             |
-//| Location of SPARQL endpoint      |
-//| Description of available formats |
+// Scenario: An anonymous user views the metadata for a registry as HTML
+//    Given that there is an existing populated entity registry with a schema
+//    When an anonymous user dereferences the base URI for the registry specifying mediatype text/html
+//    Then they see metadata related to the entity registry regarding:
+//      | Registry name                    |
+//      | Registry type                    |
+//      | Publisher                        |
+//      | License for the data             |
+//      | Owner organisation               |
+//      | Participating organisations      |
+//      | Languages used in dataset        |
+//      | Creation date                    |
+//      | Modification date                |
+//      | Relations to other data sets     |
+//      | Location of APIs                 |
+//      | Example resources                |
+//      | Base URI for dataset             |
+//      | Location of SPARQL endpoint      |
+//      | Description of available formats |
 
 When(/an anonymous user dereferences the base URI for the registry specifying mediatype text\/html/, (dataTable) => {
   cy.log('-- anonymous_user_view_entity_registry_metadata_HTML.js --');
