44ecb33a69fcfe00321862e7096650b69bbab3e2
en <en@brucia.bibsys.no>
2019-02-06 14:32:00 +0100

    Using JsonLdConsts

--- api/src/main/java/no/bibsys/web/model/EntityMarcMessageBodyWriter.java
+++ api/src/main/java/no/bibsys/web/model/EntityMarcMessageBodyWriter.java
@@ -21,6 +21,7 @@ import org.marc4j.marc.Record;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.jsonldjava.core.JsonLdConsts;
 
 import no.bibsys.aws.tools.JsonUtils;
 
@@ -28,8 +29,6 @@ import no.bibsys.aws.tools.JsonUtils;
 @Produces({CustomMediaType.APPLICATION_MARC, CustomMediaType.APPLICATION_MARCXML, CustomMediaType.APPLICATION_MARCXML_XML})
 public class EntityMarcMessageBodyWriter extends CustomMessageBodyWriter<EntityDto> {
 
-    private static final String VALUE = "@value";
-    private static final String LANGUAGE = "@language";
     private static final String LANG_EN = "en";
     private static final String LANG_NO = "no";
     private static final String PREFERRED_LABEL_FIELD = "100";
@@ -64,8 +63,8 @@ public class EntityMarcMessageBodyWriter extends CustomMessageBodyWriter<EntityD
         List<Map<String, String>> preferredLabelList = (List<Map<String, String>>) bodyMap.get("preferredLabel");
         String preferredLabelValue = "";
         for (Map<String, String> map : preferredLabelList) {
-            if(LANG_NO.equals(map.get(LANGUAGE)) || "".equals(preferredLabelValue) && LANG_EN.equals(map.get(LANGUAGE))) {
-                preferredLabelValue = map.get(VALUE);
+            if(LANG_NO.equals(map.get(JsonLdConsts.LANGUAGE)) || "".equals(preferredLabelValue) && LANG_EN.equals(map.get(JsonLdConsts.LANGUAGE))) {
+                preferredLabelValue = map.get(JsonLdConsts.VALUE);
             }
         }
         return preferredLabelValue;
