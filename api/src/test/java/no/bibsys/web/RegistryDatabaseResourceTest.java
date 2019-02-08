88ae8c4aa0e32fa43d0d6d1cc8db9de7321842f2
en <en@brucia.bibsys.no>
2019-02-08 09:35:22 +0100

    Improving tests

--- api/src/test/java/no/bibsys/web/RegistryDatabaseResourceTest.java
+++ api/src/test/java/no/bibsys/web/RegistryDatabaseResourceTest.java
@@ -3,11 +3,13 @@ package no.bibsys.web;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertThat;
 
 import java.nio.file.Paths;
 import java.util.UUID;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -18,8 +20,12 @@ import no.bibsys.web.exception.validationexceptionmappers.ShaclModelDatatypeObje
 import no.bibsys.web.model.EntityDto;
 import no.bibsys.web.model.RegistryDto;
 import no.bibsys.web.model.RegistryInfoNoMetadataDto;
+import no.bibsys.web.security.ApiKeyConstants;
 
 public class RegistryDatabaseResourceTest extends DatabaseResourceTest {
+
+    private static final String UPDATED_PUBLISHER_VALUE = "Updated publisher value";
+    private static final String PUBLISHER = "Publisher";
 
     @Test
     public void createRegistry_RegistryNotExistingUserNotAuthorized_StatusForbidden() throws Exception {
@@ -107,7 +113,7 @@ public class RegistryDatabaseResourceTest extends DatabaseResourceTest {
     }
 
     @Test
-    public void replaceApiKey_RegistryExists_ReturnsNewApiKey() throws Exception {
+    public void replaceApiKey_registryExists_returnsNewApiKey() throws Exception {
         String registryName = UUID.randomUUID().toString();
         RegistryDto registryDto = sampleData.sampleRegistryDto(registryName);
         Response createRegistryResponse = createRegistry(registryDto, apiAdminKey);
@@ -122,13 +128,13 @@ public class RegistryDatabaseResourceTest extends DatabaseResourceTest {
     }
 
     @Test
-    public void replaceApiKey_RegistryNotExisting_ReturnsStatusNotFound() throws Exception {
+    public void replaceApiKey_registryNotExisting_returnsStatusNotFound() throws Exception {
         String registryName = UUID.randomUUID().toString();
         String oldApiKey = UUID.randomUUID().toString(); // random non-existing apikey
 
         Response newApiKeyResponse = replaceApiKey(registryName, oldApiKey);
 
-        assertThat(newApiKeyResponse.getStatus(), is(equalTo(Status.NOT_FOUND.getStatusCode())));
+        assertThat(newApiKeyResponse.getStatusInfo(), is(equalTo(Status.NOT_FOUND)));
     }
 
     @Test
@@ -140,7 +146,47 @@ public class RegistryDatabaseResourceTest extends DatabaseResourceTest {
 
         Response newApiKeyResponse = replaceApiKey(registryName, oldApiKey);
 
-        assertThat(newApiKeyResponse.getStatus(), is(equalTo(Status.BAD_REQUEST.getStatusCode())));
+        assertThat(newApiKeyResponse.getStatusInfo(), is(equalTo(Status.BAD_REQUEST)));
     }
 
+    @Test
+    public void getRegistryMetadata_textHtml_registryAsHtml() throws Exception {
+        String registryName = UUID.randomUUID().toString();
+        createRegistry(registryName, apiAdminKey);
+
+        Response entityAsHtml = getRegistry(registryName, MediaType.TEXT_HTML);
+        String html = entityAsHtml.readEntity(String.class);
+
+        assertThat(html, containsString("html"));
+        assertThat(html, containsString("<title>Registry name value</title>"));
+        assertThat(html, containsString("data-automation-id=\"Registry_name\""));
+        assertThat(html, containsString("data-automation-id=\"Publisher\""));
+    }
+    
+    @Test
+    public void updateRegistryMetadata_registryExists_returnsUpdatedRegistryMetadata() throws Exception {
+        String registryName = UUID.randomUUID().toString();
+        RegistryDto registryDto = sampleData.sampleRegistryDto(registryName);
+        createRegistry(registryDto, apiAdminKey);
+
+        registryDto.getMetadata().put(PUBLISHER, UPDATED_PUBLISHER_VALUE);
+        Response updateRegistryResponse = updateRegistry(registryDto, apiAdminKey);
+        assertThat(updateRegistryResponse.getStatusInfo(), is(Status.OK));
+        
+        Response getRegistryResponse = getRegistry(registryName, MediaType.APPLICATION_JSON);
+        RegistryDto updatedRegistryDto = getRegistryResponse.readEntity(RegistryDto.class);
+
+    }
+    
+    @Test
+    public void updateRegistryMetadata_registryNotExists_returnsNotFound() {
+        
+    }
+
+    private Response updateRegistry(RegistryDto registryDto, String apiKey) {
+        Response response = target("/registry").request().accept(MediaType.APPLICATION_JSON)
+                .header(ApiKeyConstants.API_KEY_PARAM_NAME, apiKey)
+                .put(javax.ws.rs.client.Entity.entity(registryDto, MediaType.APPLICATION_JSON));
+        return response;
+    }
 }
