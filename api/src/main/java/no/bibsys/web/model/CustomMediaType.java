6d41ef0ae0e1a701ab591113177015cd4ca1e77c
en <en@brucia.bibsys.no>
2019-02-12 12:32:31 +0100

    custommediatype

--- api/src/main/java/no/bibsys/web/model/CustomMediaType.java
+++ api/src/main/java/no/bibsys/web/model/CustomMediaType.java
@@ -10,6 +10,5 @@ public class CustomMediaType {
     public static final String APPLICATION_JSON_LD = "application/ld+json";
     public static final String APPLICATION_MARC = "application/marc";
     public static final String APPLICATION_MARCXML = "application/marcxml";
-    public static final String APPLICATION_MARCXML_XML = "application/marcxml+Xml";
-
+    public static final String APPLICATION_MARCXML_XML = "application/marcxml+xml";
 }
