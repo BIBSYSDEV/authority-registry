ad0909f26c00388b8b6e2f0a54ce2ba9bdbe7667
en <en@brucia.bibsys.no>
2019-05-03 08:38:35 +0200

    AUTREG-189: Removed unused fields and constants

--- api/src/main/java/no/bibsys/service/AuthenticationService.java
+++ api/src/main/java/no/bibsys/service/AuthenticationService.java
@@ -28,28 +28,22 @@ import com.amazonaws.services.dynamodbv2.model.SSESpecification;
 import no.bibsys.EnvironmentVariables;
 import no.bibsys.aws.cloudformation.Stage;
 import no.bibsys.aws.tools.Environment;
-import no.bibsys.db.TableDriver;
 import no.bibsys.web.exception.ApiKeyTableBeingCreatedException;
 
 public class AuthenticationService {
 
-    private static final String ACTIVE = "ACTIVE";
-    private static final String UPDATING = "UPDATING";
-    private static final String CREATING = "CREATING";
     private static final Logger logger = LoggerFactory.getLogger(AuthenticationService.class);
     private final transient DynamoDBMapper mapper;
     private final transient DynamoDBMapperConfig config;
     private final transient Environment environmentReader;
     private final transient String apiKeyTableName;
     private final transient DynamoDB dynamoDB;
-    private final transient TableDriver tableDriver;
 
 
     public AuthenticationService(AmazonDynamoDB client, Environment environmentReader) {
         this.environmentReader = environmentReader;
         mapper = new DynamoDBMapper(client);
         dynamoDB = new DynamoDB(client);
-        tableDriver = new TableDriver(client);
 
         apiKeyTableName = environmentReader.readEnv(EnvironmentVariables.API_KEY_TABLE_NAME);
 
