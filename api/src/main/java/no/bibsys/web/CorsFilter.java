82097292507c51f6831f878676cc2d3996c271fa
en <en@brucia.bibsys.no>
2019-06-06 09:15:53 +0200

    Added allow-headers

--- api/src/main/java/no/bibsys/web/CorsFilter.java
+++ api/src/main/java/no/bibsys/web/CorsFilter.java
@@ -19,7 +19,7 @@ public class CorsFilter implements ContainerResponseFilter {
             "Access-Control-Allow-Credentials", "true");
           responseContext.getHeaders().add(
            "Access-Control-Allow-Headers",
-           "origin, content-type, accept, authorization");
+           "origin, content-type, accept, authorization, x-amz-date, api-key");
           responseContext.getHeaders().add(
             "Access-Control-Allow-Methods", 
             "GET, POST, PUT, DELETE, OPTIONS, HEAD");
