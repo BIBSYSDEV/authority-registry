7b77e844b2416c8c8da225646988abde0053eaa6
svenn.gjeto <svenn.gjeto@unit.no>
2019-06-17 13:50:00 +0200

    Added CloudsearchEndpoint

---
+++ dynamo/src/main/java/no/bibsys/db/CloudsearchClient.java
@@ -0,0 +1,23 @@
+package no.bibsys.db;
+
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CloudsearchClient {
+
+    private URL endpoint;
+    private static final Logger logger = LoggerFactory.getLogger(CloudsearchClient.class);
+    
+    public CloudsearchClient(URL cloudsearchDocumentEndpointUrl) {
+        super();
+        this.endpoint = cloudsearchDocumentEndpointUrl;
+        logger.debug("CloudsearchClient setting Cs endpoint to ={}", cloudsearchDocumentEndpointUrl);
+    }
+
+    public void upsert(String eventSource) {
+        logger.debug("updating CloudSearch@{} with record = {}" + eventSource, endpoint, eventSource);
+    }
+
+}
--- dynamo/src/main/java/no/bibsys/db/DynamoDBEventProcessor.java
+++ dynamo/src/main/java/no/bibsys/db/DynamoDBEventProcessor.java
@@ -1,5 +1,8 @@
 package no.bibsys.db;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,7 +13,23 @@ import com.amazonaws.services.lambda.runtime.events.DynamodbEvent.DynamodbStream
 
 public class DynamoDBEventProcessor implements RequestHandler<DynamodbEvent, Void> {
 
+    private static final String CLOUDSEARCH_DOCUMENT_ENDPOINT = "CLOUDSEARCH_DOCUMENT_ENDPOINT";
     private static final Logger logger = LoggerFactory.getLogger(DynamoDBEventProcessor.class);
+    CloudsearchClient cloudsearchClient;
+    
+    
+    
+    public DynamoDBEventProcessor() {
+        super();
+        String cloudsearchDomainName = System.getenv(CLOUDSEARCH_DOCUMENT_ENDPOINT);
+        try {
+            URL cloudsearchDocumentEndpointUrl = new URL(cloudsearchDomainName);
+            this.cloudsearchClient = new CloudsearchClient(cloudsearchDocumentEndpointUrl);
+        } catch (MalformedURLException e) {
+            logger.error("",e);
+        }
+    }
+
     
     @Override
     public Void handleRequest(DynamodbEvent dynamodbEvent, Context context) {
@@ -28,7 +47,9 @@ public class DynamoDBEventProcessor implements RequestHandler<DynamodbEvent, Voi
             logger.debug("record.eventID={}, record.eventName={}, record.dynamodb={}", 
                     record.getEventID(), record.getEventName(), record.getDynamodb());
             if (record.getEventName().equals("MODIFY")) {
-                System.out.println("updating ALMA for record =" + record.getEventSource());        
+                cloudsearchClient .upsert(record.getEventSource());
+            } else {
+                logger.debug("handleRequest doesn't know what to do with eventName={}", record.getEventName());
             }
         }
 
