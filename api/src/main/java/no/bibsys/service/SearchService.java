c2c339bc6189afc917113d3f5a76f76b2fedd5b0
svenngjeto <svenn.gjeto@unit.no>
2019-08-19 15:18:49 +0200

    reading entity from cs

--- api/src/main/java/no/bibsys/service/SearchService.java
+++ api/src/main/java/no/bibsys/service/SearchService.java
@@ -15,28 +15,33 @@ import com.amazonaws.services.cloudsearchdomain.model.QueryParser;
 import com.amazonaws.services.cloudsearchdomain.model.SearchException;
 import com.amazonaws.services.cloudsearchdomain.model.SearchRequest;
 import com.amazonaws.services.cloudsearchdomain.model.SearchResult;
+import com.fasterxml.jackson.core.JsonParser.Feature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import no.bibsys.EnvironmentVariables;
 import no.bibsys.aws.tools.Environment;
+import no.bibsys.db.structures.Entity;
+import no.bibsys.utils.JsonUtils;
 
+@SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
 public class SearchService {
 
     private static final String FILTERQUERY_BASE = "inscheme:'%s'";
     private static final String CLOUDSEARCH_RETURN_FIELD = "presentation_json";
-    private transient final String serviceEndpoint;
-    private static final Logger logger = LoggerFactory.getLogger(SearchService.class);
-
     private static final String AWS_REGION_PROPERTY_NAME = "AWS_REGION";
+    
+    private static final Logger logger = LoggerFactory.getLogger(SearchService.class);
+    
+    private transient final String serviceEndpoint;
     private final transient AmazonCloudSearchDomain searchClient;
-
 
     public SearchService(Environment environmentReader) {
         this.serviceEndpoint = environmentReader.readEnv(EnvironmentVariables.CLOUDSEARCH_SEARCH_ENDPOINT);
 
         String signingRegion = System.getenv(AWS_REGION_PROPERTY_NAME);
 
-        logger.debug("searchEndpoint={}", this.serviceEndpoint);
-        logger.debug("SearchService.serviceEndpoint='{}', signingRegion='{}'", serviceEndpoint, signingRegion);
+        logger.debug("SearchService.serviceEndpoint='{}', signingRegion='{}'", this.serviceEndpoint, signingRegion);
 
         EndpointConfiguration endpointConfiguration = new EndpointConfiguration(serviceEndpoint, signingRegion);
         searchClient = AmazonCloudSearchDomainClientBuilder.standard()
@@ -45,7 +50,7 @@ public class SearchService {
 
     }
 
-    public List<String> simpleQuery(String registryName, String queryString) {
+    public List<Entity> simpleQuery(String registryName, String queryString) {
         logger.debug("Searching, endpoint={}, registryName={}, queryString={}", 
                 this.serviceEndpoint, registryName, queryString);
         
@@ -57,14 +62,21 @@ public class SearchService {
                 .withQueryParser(QueryParser.Simple);
         try {
             logger.debug("searchRequest={}", searchRequest);
-            List<String> result = new ArrayList<>();
+            List<Entity> result = new ArrayList<>();
             SearchResult searchResult = searchClient.search(searchRequest);
             logger.debug("searchResult={}", searchResult);
              Hits hits = searchResult.getHits();
+             ObjectMapper objectMapper = JsonUtils.newJsonParser();
+             objectMapper.configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
              for (Hit hit : hits.getHit()) {
-                result.addAll(hit.getFields().get(CLOUDSEARCH_RETURN_FIELD));
+                 try {
+                     Entity entity = new Entity();
+                     entity.setBody((ObjectNode)objectMapper.readTree(hit.getFields().get(CLOUDSEARCH_RETURN_FIELD).toString()));
+                     result.add(entity);
+                 } catch (Exception e) {
+                     logger.error("",e);
+                 }
             }
-             logger.debug("result={}", result);
             return result;
         } catch (SearchException e) {
             logger.error("",e);
