c286042eb80161cca345bb73c68b7d07a49bce45
brinxmat <rurik.greenall@gmail.com>
2019-08-26 10:51:02 +0200

    Remove unused logger

--- api/src/main/java/no/bibsys/web/DatabaseResource.java
+++ api/src/main/java/no/bibsys/web/DatabaseResource.java
@@ -27,8 +27,6 @@ import no.bibsys.web.model.RegistryDto;
 import no.bibsys.web.model.RegistryInfoNoMetadataDto;
 import no.bibsys.web.security.ApiKeyConstants;
 import no.bibsys.web.security.Roles;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
@@ -72,7 +70,6 @@ public class DatabaseResource {
     private static final String REGISTRY_NAME = "registryName";
     private static final String REGISTRY = "registry";
 
-    private static final Logger logger = LoggerFactory.getLogger(DatabaseResource.class);
     private final transient RegistryService registryService;
     private final transient EntityService entityService;
     private final transient SearchService searchService;
