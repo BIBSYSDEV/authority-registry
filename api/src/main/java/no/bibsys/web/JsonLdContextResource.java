790837eac0f951c3ab25d23e9eac998b609eecd4
brinxmat <rurik.greenall@gmail.com>
2019-09-04 21:39:26 +0200

    Move path annotation to class

--- api/src/main/java/no/bibsys/web/JsonLdContextResource.java
+++ api/src/main/java/no/bibsys/web/JsonLdContextResource.java
@@ -10,13 +10,13 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+@Path("/json-ld/context")
 public class JsonLdContextResource {
 
     private static final String CHARSET_UTF_8 = ";charset=utf-8";
     private static final String JSON_LD_CONTEXT_JSON = "/json_ld_context.json";
-    private static final String JSON_LD_CONTEXT = "/json-ld/context";
 
-    @Path(JSON_LD_CONTEXT)
+    @Path("/")
     @GET()
     @Produces(CustomMediaType.APPLICATION_JSON_LD + CHARSET_UTF_8)
     public Response getJsonLdContext() throws IOException {
